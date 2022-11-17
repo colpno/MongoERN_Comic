791e0b68bb19b14436529efc4899a5722e4180b6
Gia Vinh <gvinhh@gmail.com>
2022-11-17 16:37:42 +0700

    Convert query string to sql statement

---
+++ server/controllers/common/convertToSQL.js
@@ -0,0 +1,70 @@
+export const convertToSQL = (queries) => {
+  const { sort, order, limit, page, embed, ...otherQueries } = queries;
+  const queryKeys = Object.keys(otherQueries);
+
+  const whereArray = [];
+  const values = [];
+
+  for (let i = 0; i < queryKeys.length; i++) {
+    const query = queryKeys[i];
+
+    if (otherQueries[query].length === 0) {
+      continue;
+    }
+
+    if (typeof otherQueries[query] === typeof []) {
+      values.push(...otherQueries[query]);
+      whereArray.push(...otherQueries[query].map(() => `${query} = ?`));
+      continue;
+    }
+
+    values.push(otherQueries[query]);
+
+    if (query.includes('_gte')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_gte'))}\` >= ?`);
+      continue;
+    }
+
+    if (query.includes('_lte')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_lte'))}\` <= ?`);
+      continue;
+    }
+
+    if (query.includes('_gt')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_gt'))}\` > ?`);
+      continue;
+    }
+
+    if (query.includes('_lt')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_lt'))}\` < ?`);
+      continue;
+    }
+
+    if (query.includes('_ne')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_ne'))}\` <> ?`);
+      continue;
+    }
+
+    if (query.includes('_like')) {
+      whereArray.push(`\`${query.slice(0, query.indexOf('_like'))}\` LIKE '%?%'`);
+      continue;
+    }
+
+    whereArray.push(`\`${query}\` = ?`);
+  }
+
+  const whereStatement = whereArray.reduce(
+    (str, sql, index) => (index === 0 ? `WHERE${str} ${sql}` : `${str} AND ${sql}`),
+    ''
+  );
+
+  const orderStatement =
+    sort &&
+    order &&
+    `ORDER BY
+    \`${sort}\`+0 ${order.toUpperCase()}, \`${sort}\` ${order.toUpperCase()}`;
+
+  const limitStatement = page && limit && `LIMIT ${(page - 1) * limit},${limit * page}`;
+
+  return { whereStatement, orderStatement, limitStatement, values };
+};
