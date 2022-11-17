2ee8a6dc91d84175d140ffa89195947308fa7608
Gia Vinh <gvinhh@gmail.com>
2022-11-17 17:37:53 +0700

    Toggle sidebar make change UI, clear search box every page change

--- client/src/layouts/AdminLayout/components/AdminSideBar.jsx
+++ client/src/layouts/AdminLayout/components/AdminSideBar.jsx
@@ -1,54 +1,68 @@
 import classNames from "classnames/bind";
-import PropTypes from "prop-types";
+import { FaBars } from "react-icons/fa";
 import { IoIosLogOut } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Logo } from "assets/images";
 import { Button, Image } from "components";
 import { SideBar } from "features";
 import { useLogout } from "hooks";
-import { BsLayoutSidebarInset } from "react-icons/bs";
-import { useSelector } from "react-redux";
+import { setSearchText, toggleSideBar } from "libs/redux/slices/globalSlice";
 import getAdminSideBarMenu from "utils/constants";
 import styles from "../styles/AdminSideBar.module.scss";
 
 const cx = classNames.bind(styles);
 
-function AdminSideBar({ visible, setToggleSideBar, toggleSideBar }) {
+function AdminSideBar() {
+  const dispatch = useDispatch();
+  const searchText = useSelector((state) => state.global.searchText);
+  const toggle = useSelector((state) => state.global.toggleSideBar);
   const menu = getAdminSideBarMenu();
   const user = useSelector((state) => state.user.user);
   const { logout } = useLogout("/");
 
+  if (searchText.length > 0) {
+    dispatch(setSearchText(""));
+  }
+
+  const setToggleSideBar = (isToggle) => {
+    dispatch(toggleSideBar(isToggle));
+  };
+
   return (
-    <aside className={cx("sidebar", visible ? "" : "visible")}>
-      <Button wrapper to="/" className={cx("logo-wrapper")}>
-        <Logo className={cx("logo")} />
-      </Button>
-      <BsLayoutSidebarInset
-        className={cx("sidebar--toggle")}
-        onClick={() => setToggleSideBar(!toggleSideBar)}
-      />
-      <SideBar menu={menu} defaultTab={menu[0].subMenu[0].tab} />
-      <div className={cx("user-info")}>
-        <Image
-          src={user.avatar}
-          alt="User avatar"
-          className={cx("avatar")}
-          width={50}
-          height={50}
-        />
-        <span className={cx("username")}>{user.username}</span>
-        <Button onClick={logout}>
-          <IoIosLogOut className={cx("logout-icon")} />
-        </Button>
-      </div>
-    </aside>
+    <div className={cx("sidebar-container", toggle ? "active" : "")}>
+      <div className={cx("dump", toggle ? "active" : "")} />
+      <aside className={`sidebar ${toggle ? "active" : ""}`}>
+        <div className={cx("logo-container")}>
+          <Button wrapper to="/" className={cx("logo-button")}>
+            <Logo className={cx("logo")} />
+          </Button>
+          <FaBars
+            className={cx("sidebar--toggle")}
+            onClick={() => setToggleSideBar(!toggle)}
+          />
+        </div>
+        <div className={cx("menu")}>
+          <SideBar menu={menu} defaultTab={menu[0].subMenu[0].tab} />
+        </div>
+        <div className={cx("user-info")}>
+          <Image
+            src={user.avatar}
+            alt="User avatar"
+            className={cx("avatar")}
+            width={60}
+            height={60}
+          />
+          <span className={cx("username")}>{user.username}</span>
+          <Button wrapper onClick={logout} className={cx("logout")}>
+            <IoIosLogOut className={cx("icon")} />
+          </Button>
+        </div>
+      </aside>
+    </div>
   );
 }
 
-AdminSideBar.propTypes = {
-  visible: PropTypes.bool.isRequired,
-  setToggleSideBar: PropTypes.func.isRequired,
-  toggleSideBar: PropTypes.bool.isRequired,
-};
+AdminSideBar.propTypes = {};
 
 export default AdminSideBar;
