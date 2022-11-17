a377a5b3a60c659d3c6370c1317ac3a6e505ddb4
Gia Vinh <gvinhh@gmail.com>
2022-11-17 17:21:35 +0700

    Create choose avatar container

--- client/src/pages/Profile/components/AvatarBox.jsx
+++ client/src/pages/Profile/components/AvatarBox.jsx
@@ -1,73 +1,134 @@
-import { Col, Container, Row } from "react-bootstrap";
 import classNames from "classnames/bind";
+import { Col } from "react-bootstrap";
+import PropTypes from "prop-types";
 
+import {
+  cat1,
+  cat10,
+  cat2,
+  cat3,
+  cat4,
+  cat5,
+  cat6,
+  cat7,
+  cat8,
+  cat9,
+  monster1,
+  monster10,
+  monster2,
+  monster3,
+  monster4,
+  monster5,
+  monster6,
+  monster7,
+  monster8,
+  monster9,
+  robot1,
+  robot10,
+  robot2,
+  robot3,
+  robot4,
+  robot5,
+  robot6,
+  robot7,
+  robot8,
+  robot9,
+  robotHead1,
+  robotHead10,
+  robotHead2,
+  robotHead3,
+  robotHead4,
+  robotHead5,
+  robotHead6,
+  robotHead7,
+  robotHead8,
+  robotHead9,
+} from "assets/images";
 import { Image } from "components";
 import styles from "../styles/AvatarBox.module.scss";
 
 const cx = classNames.bind(styles);
 
-function AvatarBox() {
+function AvatarBox({ value, handleOnChange }) {
   const avatars = [
-    "https://robohash.org/quisuntquia.png?size=200x200&set=set1",
-    "https://robohash.org/estcorporisminus.png?size=200x200&set=set1",
-    "https://robohash.org/placeatducimusaut.png?size=200x200&set=set1",
-    "https://robohash.org/recusandaeutet.png?size=200x200&set=set1",
-    "https://robohash.org/quibusdametquis.png?size=200x200&set=set1",
-    "https://robohash.org/adarchitectodolorum.png?size=200x200&set=set1",
-    "https://robohash.org/etrecusandaequam.png?size=200x200&set=set1",
-    "https://robohash.org/harumculpaut.png?size=200x200&set=set1",
-    "https://robohash.org/possimusexercitationemaut.png?size=200x200&set=set1",
-    "https://robohash.org/sequiautoccaecati.png?size=200x200&set=set1",
+    robot1,
+    robot2,
+    robot3,
+    robot4,
+    robot5,
+    robot6,
+    robot7,
+    robot8,
+    robot9,
+    robot10,
 
-    "https://robohash.org/adipisciearumaut.png?size=200x200&set=set2",
-    "https://robohash.org/aliasamaxime.png?size=200x200&set=set2",
-    "https://robohash.org/numquamdoloremculpa.png?size=200x200&set=set2",
-    "https://robohash.org/delenitiearumut.png?size=200x200&set=set2",
-    "https://robohash.org/eummollitiaprovident.png?size=200x200&set=set2",
-    "https://robohash.org/etsapientesunt.png?size=200x200&set=set2",
-    "https://robohash.org/etenimest.png?size=200x200&set=set2",
-    "https://robohash.org/voluptascummagnam.png?size=200x200&set=set2",
-    "https://robohash.org/sitsimiliquelaborum.png?size=200x200&set=set2",
-    "https://robohash.org/possimusvitaeet.png?size=200x200&set=set2",
+    robotHead1,
+    robotHead2,
+    robotHead3,
+    robotHead4,
+    robotHead5,
+    robotHead6,
+    robotHead7,
+    robotHead8,
+    robotHead9,
+    robotHead10,
 
-    "https://robohash.org/adipisciearumaut.png?size=200x200&set=set3",
-    "https://robohash.org/aliasamaxime.png?size=200x200&set=set3",
-    "https://robohash.org/numquamdoloremculpa.png?size=200x200&set=set3",
-    "https://robohash.org/delenitiearumut.png?size=200x200&set=set3",
-    "https://robohash.org/eummollitiaprovident.png?size=200x200&set=set3",
-    "https://robohash.org/etsapientesunt.png?size=200x200&set=set3",
-    "https://robohash.org/etenimest.png?size=200x200&set=set3",
-    "https://robohash.org/voluptascummagnam.png?size=200x200&set=set3",
-    "https://robohash.org/sitsimiliquelaborum.png?size=200x200&set=set3",
-    "https://robohash.org/possimusvitaeet.png?size=200x200&set=set3",
+    monster1,
+    monster2,
+    monster3,
+    monster4,
+    monster5,
+    monster6,
+    monster7,
+    monster8,
+    monster9,
+    monster10,
 
-    "https://robohash.org/adipisciearumaut.png?size=200x200&set=set4",
-    "https://robohash.org/aliasamaxime.png?size=200x200&set=set4",
-    "https://robohash.org/numquamdoloremculpa.png?size=200x200&set=set4",
-    "https://robohash.org/delenitiearumut.png?size=200x200&set=set4",
-    "https://robohash.org/eummollitiaprovident.png?size=200x200&set=set4",
-    "https://robohash.org/etsapientesunt.png?size=200x200&set=set4",
-    "https://robohash.org/etenimest.png?size=200x200&set=set4",
-    "https://robohash.org/voluptascummagnam.png?size=200x200&set=set4",
-    "https://robohash.org/sitsimiliquelaborum.png?size=200x200&set=set4",
-    "https://robohash.org/possimusvitaeet.png?size=200x200&set=set4",
+    cat1,
+    cat2,
+    cat3,
+    cat4,
+    cat5,
+    cat6,
+    cat7,
+    cat8,
+    cat9,
+    cat10,
   ];
   return (
-    <Container>
-      <Row className={cx("avatars-container")}>
-        {avatars.map((avatar, index) => (
+    <div className={cx("avatars-container")}>
+      {avatars.map((avatar, index) => {
+        const alt = avatar.slice(
+          avatar.lastIndexOf("/") + 1,
+          avatar.indexOf(".")
+        );
+
+        return (
           <Col sm={20} key={index}>
+            <input
+              name="avatar"
+              onChange={handleOnChange}
+              type="radio"
+              value={avatar}
+              className={cx("radio")}
+            />
             <Image
               src={avatar}
-              alt={`Avatar ${index + 1}`}
-              width={50}
-              height={50}
+              alt={`Avatar ${alt}`}
+              width={70}
+              height={70}
+              className={cx("avatar", value === avatar ? "active" : "")}
             />
           </Col>
-        ))}
-      </Row>
-    </Container>
+        );
+      })}
+    </div>
   );
 }
+
+AvatarBox.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default AvatarBox;
