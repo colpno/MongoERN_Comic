81421bc28480b5000fc521466f54bf098b01c24f
Gia Vinh <gvinhh@gmail.com>
2022-11-18 11:54:48 +0700

    Make banner using top 5 titles cover

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,27 +1,34 @@
+/* eslint-disable no-unused-vars */
 import classNames from "classnames/bind";
 import { Container } from "react-bootstrap";
 
-import { Fullsize } from "assets/images";
 import { BannerSlider } from "features";
 import styles from "pages/Home/assets/styles/Home.module.scss";
+import { useMemo } from "react";
+import { sortTitles } from "services/title";
 import { ComicSection, Notification } from "./components";
 
 const cx = classNames.bind(styles);
 
 function Home() {
-  const bannerImages = [Fullsize, Fullsize, Fullsize, Fullsize, Fullsize];
+  // const bannerImages = [Fullsize, Fullsize, Fullsize, Fullsize, Fullsize];
+  const { titles } = sortTitles("like", false, 5);
+  const bannerImages = useMemo(
+    () => titles.map((title) => title.cover),
+    [titles]
+  );
 
   return (
     <main className={cx("home")}>
       <BannerSlider images={bannerImages} />
       <section className={cx("section-comic")}>
-        <Container>
+        {/* <Container>
           <h1 className={cx("title", "line-clamp")}>Comic</h1>
-        </Container>
+        </Container> */}
         <ComicSection />
       </section>
-      <Notification />
-      <section className={cx("section-novel")}>
+      {/* <Notification /> */}
+      {/* <section className={cx("section-novel")}>
         <Container>
           <h1 className={cx("title")}>Novels</h1>
         </Container>
@@ -36,7 +43,7 @@ function Home() {
         <div className={cx("anime-action")} />
         <div className={cx("anime-horror")} />
         <div className={cx("anime-ranking")} />
-      </section>
+      </section> */}
     </main>
   );
 }
