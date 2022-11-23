1134e62c215f865be87f6e30264e31bea06185de
Gia Vinh <gvinhh@gmail.com>
2022-11-23 23:37:40 +0700

    re-export controllers

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,5 +1,8 @@
 export { default as login } from './login.js';
+export { default as verifyLoginOTP } from './verifyLoginOTP.js';
+export { default as sendOTP } from './sendOTP.js';
 export { default as register } from './register.js';
+export { default as verifyAccount } from './verifyAccount.js';
 export { default as logout } from './logout.js';
 export { default as forgotPassword } from './forgotPassword.js';
 export { default as resetPassword } from './resetPassword.js';
