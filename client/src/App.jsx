6e3b7f89d24893e40d696616e1ca63e9f69620ed
Gia Vinh <gvinhh@gmail.com>
2022-11-23 23:35:47 +0700

    prevent user access to not have permission routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,110 @@
+import { Popup } from "features";
 import { DefaultLayout } from "layouts";
-import { Fragment } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { privateRoutes, publicRoutes } from "routes";
 
+const checkLoggedInCanAccessURL = (url) => {
+  const array = ["login", "register", "reset-password", "forgot-password"];
+
+  const haveAccessed = array.some((pathName) => url.includes(pathName));
+  return haveAccessed;
+};
+
 function App() {
+  const navigate = useNavigate();
+  const [popup, setPopup] = useState({
+    trigger: false,
+    title: "Thông báo",
+    content: "",
+    isClosed: false,
+  });
   const userState = useSelector((state) => state.user);
   const { isLoggingIn } = userState;
+  const url = useLocation().pathname;
+  const haveAccessed = useMemo(() => checkLoggedInCanAccessURL(url), [url]);
+
+  useEffect(() => {
+    if (haveAccessed && isLoggingIn) {
+      setPopup((prev) => ({
+        ...prev,
+        trigger: true,
+        content: "Bạn đã đăng nhập nên không thể truy cập vào trang",
+      }));
+    }
+  }, [url]);
+
+  useEffect(() => {
+    popup.isClosed && navigate("/");
+  }, [popup.isClosed]);
 
   return (
-    <Routes>
-      {publicRoutes.map((route, index) => {
-        const { path, layout } = route;
-        const Component = route.component;
-        let Layout = DefaultLayout;
+    <>
+      <Routes>
+        {publicRoutes.map((route, index) => {
+          const { path, layout } = route;
+          const Component = route.component;
+          let Layout = DefaultLayout;
 
-        if (layout) {
-          Layout = layout;
-        } else if (layout === null) {
-          Layout = Fragment;
-        }
+          if (layout) {
+            Layout = layout;
+          } else if (layout === null) {
+            Layout = Fragment;
+          }
 
-        return (
-          <Route
-            path={path}
-            key={index}
-            element={
-              <Layout>
-                <Component />
-              </Layout>
-            }
-          />
-        );
-      })}
-      {privateRoutes.map((route, index) => {
-        const { path, layout } = route;
-        const Component = route.component;
-        let Layout = DefaultLayout;
-
-        if (layout) {
-          Layout = layout;
-        } else if (layout === null) {
-          Layout = Fragment;
-        }
-
-        return (
-          <Route
-            path={path}
-            key={index}
-            element={
-              isLoggingIn ? (
+          return (
+            <Route
+              path={path}
+              key={index}
+              element={
                 <Layout>
                   <Component />
                 </Layout>
-              ) : (
-                <Navigate to="/login" />
-              )
+              }
+            />
+          );
+        })}
+
+        {isLoggingIn &&
+          privateRoutes.map((route, index) => {
+            const { path, layout } = route;
+            const Component = route.component;
+            let Layout = DefaultLayout;
+
+            if (layout) {
+              Layout = layout;
+            } else if (layout === null) {
+              Layout = Fragment;
             }
-          />
-        );
-      })}
-      <Route path="*" element={<Navigate to="/register" />} />
-    </Routes>
+
+            return (
+              <Route
+                path={path}
+                key={index}
+                element={
+                  isLoggingIn ? (
+                    <Layout>
+                      <Component />
+                    </Layout>
+                  ) : (
+                    <Navigate to="/login" />
+                  )
+                }
+              />
+            );
+          })}
+
+        <Route path="*" element={<Navigate to="/not-found" />} />
+      </Routes>
+      <Popup popup={popup} setPopup={setPopup} />
+    </>
   );
 }
 
