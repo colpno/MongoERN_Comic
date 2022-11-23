6eb035f94bb1b99be8c61a8aabbece4a298e5ac3
Gia Vinh <gvinhh@gmail.com>
2022-11-23 23:34:50 +0700

    create replace string function

--- client/src/utils/stringMethods.js
+++ client/src/utils/stringMethods.js
@@ -1,3 +1,7 @@
 export function convertToString(number) {
   return `${number}`;
 }
+
+export function replaceAt(string, from, to, replacement) {
+  return string.substring(0, from) + replacement + string.substring(to);
+}
