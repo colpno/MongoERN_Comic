fe80a56949cbdb5372490f3738864a96ca1528cb
Gia Vinh <gvinhh@gmail.com>
2022-11-25 00:33:11 +0700

    get reading history will get along with title, chp

--- client/src/pages/ReadingHistory/ReadingHistory.jsx
+++ client/src/pages/ReadingHistory/ReadingHistory.jsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 
 import { NoData, Pagination } from "features";
 import { getLimitedReadingHistoriesByUserID } from "services/readingHistory";
-import { countdown } from "utils/convertTime";
 import ReadingHistoryTable from "./components/ReadingHistoryTable";
 import styles from "./ReadingHistory.module.scss";
 
@@ -16,39 +15,11 @@ function ReadingHistory() {
     getLimitedReadingHistoriesByUserID(user.guid, 25);
   const hasData = readingHistories.length > 0;
 
-  const calculateCountdown = (date) => {
-    const timeObj = countdown(new Date() - new Date(date));
-    const year = timeObj.year > 0 ? `${timeObj.year} năm` : "";
-    const day = timeObj.day > 0 ? `${timeObj.day} ngày` : "";
-    const hour = timeObj.hour > 0 ? `${timeObj.hour} giờ` : "";
-    const minute = timeObj.minute > 0 ? `${timeObj.minute} phút` : "";
-    const second = timeObj.second > 0 ? `${timeObj.second} giây` : "";
-
-    if (year) {
-      return `${year} ${day}`;
-    }
-    if (day) {
-      return `${day} ${hour}`;
-    }
-    if (hour) {
-      return `${hour} ${minute}`;
-    }
-    if (minute) {
-      return `${minute} ${second}`;
-    }
-
-    return second;
-  };
-
   return (
     <>
       {hasData ? (
         <Container className={cx("reading-history")}>
-          <ReadingHistoryTable
-            readingHistories={readingHistories}
-            cx={cx}
-            calculateCountdown={calculateCountdown}
-          />
+          <ReadingHistoryTable readingHistories={readingHistories} cx={cx} />
           <Pagination pagination={pagination} setPagination={setPagination} />
         </Container>
       ) : (
