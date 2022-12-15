25fd82aa088760940e80106025c4e60e930cd6ca
Gia Vinh <gvinhh@gmail.com>
2022-12-15 09:55:45 +0700

    change call api structure

--- client/src/pages/CreateChapter/CreateChapter.jsx
+++ client/src/pages/CreateChapter/CreateChapter.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import * as Yup from "yup";
 
 import ChapterForm from "components/ChapterForm";
 import FormWrapper from "components/FormWrapper/FormWrapper";
 import { Popup, ProgressCircle } from "features";
 import { useToast } from "hooks";
-import { addChapter, sortChapters } from "services/chapter";
+import { addChapter, getAllChapters } from "services/chapter";
+import { createChapterFormValidation } from "validations";
 
 function CreateChapter() {
-  const [progress, setProgress] = useState(0);
   const { titleId } = useParams();
+  const [progress, setProgress] = useState(0);
   const { Toast, options: toastOptions, toastEmitter } = useToast();
+  const [chapters, setChapters] = useState([]);
   const [popup, setPopup] = useState({
     trigger: false,
     title: "Thông báo",
     content: "",
   });
-  const { chapters, refetch } = sortChapters(titleId, "order", false, 1);
 
   const INITIAL_VALUE = {
     order: `${Number.parseInt(chapters[0]?.order || 0, 10) + 1}`,
@@ -27,15 +27,17 @@ function CreateChapter() {
     images: [],
   };
 
-  const VALIDATION_SCHEMA = Yup.object({
-    order: Yup.number(),
-    name: Yup.string()
-      .max(255, "Giới hạn độ dài là 255 ký tự.")
-      .required("Tiêu đề không được để trống."),
-    cost: Yup.string().required("Chương tối thiểu là miễn phí"),
-    cover: Yup.string().required("Ảnh bìa không được để trống."),
-    images: Yup.array().of(Yup.string()).min(1, "Nội dung không được để trống"),
-  });
+  const fetchData = () => {
+    const params = {
+      titleId,
+      sort: "order",
+      order: "desc",
+      limit: 1,
+    };
+    getAllChapters(params)
+      .then((response) => setChapters(response.data))
+      .catch((error) => console.log(error));
+  };
 
   const handleSubmit = (
     { cover, images, ...values },
@@ -53,7 +55,7 @@ function CreateChapter() {
           toastEmitter("Truyện đã được thêm thành công", "success");
           setProgress(0);
           resetForm();
-          refetch();
+          fetchData();
         }
       })
       .catch((error) => {
@@ -64,12 +66,16 @@ function CreateChapter() {
     setSubmitting(false);
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <FormWrapper title="Thêm chương mới">
         <ChapterForm
           initialValues={INITIAL_VALUE}
-          validationSchema={VALIDATION_SCHEMA}
+          validationSchema={createChapterFormValidation}
           handleSubmit={handleSubmit}
         />
       </FormWrapper>
