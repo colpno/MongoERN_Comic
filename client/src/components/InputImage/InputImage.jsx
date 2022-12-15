2518f776663cc3d8ff157893d98dcbc6b842125b
Gia Vinh <gvinhh@gmail.com>
2022-12-15 09:33:56 +0700

    correct import path

--- client/src/components/InputImage/InputImage.jsx
+++ client/src/components/InputImage/InputImage.jsx
@@ -1,9 +1,9 @@
-import usePreviewImage from "hooks/usePreviewImage";
 import PropTypes from "prop-types";
 import { FiUpload } from "react-icons/fi";
 import { IoCloseCircle } from "react-icons/io5";
 
 import classNames from "classnames/bind";
+import { usePreviewImage } from "hooks";
 import styles from "./InputImage.scss";
 
 const cx = classNames.bind(styles);
