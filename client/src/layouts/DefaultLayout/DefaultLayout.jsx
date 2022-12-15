8dd153b7cbd31f6d1797e128a6cedfb2e98fe0a7
Gia Vinh <gvinhh@gmail.com>
2022-12-15 14:51:38 +0700

    add toggle sub navbar

--- client/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ client/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,24 +1,31 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 import Footer from "layouts/components/Footer";
 import Header from "layouts/components/Header";
-// REMOVE: import SubNavigator from "layouts/components/SubNavigator";
+import SubNavigator from "layouts/components/SubNavigator";
 
 function DefaultLayout({ children }) {
-  /* REMOVE
-   const headerNavigation = [
-     { href: "/", label: "Trang chủ" },
-     { href: "/comic/weekly", label: "Hàng tuần" },
-     { href: "/comic/ranking", label: "Xếp hạng" },
-     { href: "/comic/complete", label: "Truyện hoàn thành" },
-   ];
-   */
+  const [toggleMobileNavbar, setToggleMobileNavbar] = useState(false);
+
+  const headerNavigation = [
+    { href: "/", label: "Trang chủ" },
+    { href: "/comic/weekly", label: "Hàng tuần" },
+    { href: "/comic/ranking", label: "Xếp hạng" },
+    { href: "/comic/complete", label: "Truyện hoàn thành" },
+  ];
+
+  const handleToggleMobileNavbar = () => {
+    setToggleMobileNavbar((prev) => !prev);
+  };
+
+  console.log(toggleMobileNavbar);
 
   return (
     <>
-      <Header />
+      <Header toggleMobileNavbar={handleToggleMobileNavbar} />
       <div className="content skip-header">
-        {/* REMOVE: <SubNavigator menu={headerNavigation} /> */}
+        <SubNavigator menu={headerNavigation} />
         {children}
       </div>
       <Footer />
