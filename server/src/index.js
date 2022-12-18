d1b19d19ff824349d68dad78ca0aedd65b40035d
Colpno <gvinhh@gmail.com>
2022-12-18 21:27:24 +0700

    add socket and move routers, midwares

--- server/src/index.js
+++ server/src/index.js
@@ -1,72 +1,31 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import { config } from 'dotenv';
 import express from 'express';
-import {
-  authRoute,
-  chapterImageRoute,
-  chapterRoute,
-  purchasedChapterRoute,
-  coinTransactionRoute,
-  followRoute,
-  genreRoute,
-  paymentMethodRoute,
-  readingHistoryRoute,
-  approvedStatusRoute,
-  titleGenreRoute,
-  titleReportRoute,
-  titleRoute,
-  userRoute,
-  chargeRoute,
-  userLikeRoute,
-  hiredChapterRoute,
-} from './routes/index.js';
+import http from 'http';
+import { Server } from 'socket.io';
+
+import { handleConnection } from './controllers/socket/index.js';
+import { handleMiddleWares } from './middlewares/index.js';
+import { routeDivider } from './routes/index.js';
 
 config();
+
+const { PORT, SERVER_URL, CLIENT_URL, ADMIN_URL } = process.env;
+const SOCKET_SERVER_CONFIG = { cors: { origin: [CLIENT_URL, ADMIN_URL] } };
+
 const app = express();
-const { PORT, BASE_URL, CLIENT_URL, ADMIN_URL } = process.env;
-// [ process.env.CLIENT_URL, 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html', ]
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', [CLIENT_URL, ADMIN_URL]);
+const server = http.createServer(app);
+const socketIO = new Server(server, SOCKET_SERVER_CONFIG);
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type: application/json',
-    'Content-Type: multipart/form-data'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+// Middle wares
+handleMiddleWares(app);
 
-  next();
-});
-app.use(express.json({ limit: '50mb' }));
-app.use(cookieParser());
-app.use(
-  cors({
-    // origin: [CLIENT_URL, 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'],
-    origin: [CLIENT_URL, ADMIN_URL],
-  })
-);
+// Routes
+routeDivider(app);
 
-app.use(`${BASE_URL}/chapters`, chapterRoute);
-app.use(`${BASE_URL}/purchased-chapters`, purchasedChapterRoute);
-app.use(`${BASE_URL}/hired-chapters`, hiredChapterRoute);
-app.use(`${BASE_URL}/chapter-images`, chapterImageRoute);
-app.use(`${BASE_URL}/coin-transactions`, coinTransactionRoute);
-app.use(`${BASE_URL}/follows`, followRoute);
-app.use(`${BASE_URL}/genres`, genreRoute);
-app.use(`${BASE_URL}/payment-methods`, paymentMethodRoute);
-app.use(`${BASE_URL}/reading-histories`, readingHistoryRoute);
-app.use(`${BASE_URL}/approved-statuses`, approvedStatusRoute);
-app.use(`${BASE_URL}/titles`, titleRoute);
-app.use(`${BASE_URL}/title-genres`, titleGenreRoute);
-app.use(`${BASE_URL}/title-reports`, titleReportRoute);
-app.use(`${BASE_URL}/users`, userRoute);
-app.use(`${BASE_URL}/user-like`, userLikeRoute);
-app.use(`${BASE_URL}/auth`, authRoute);
-app.use(`${BASE_URL}/charge`, chargeRoute);
+// Socket
+socketIO.on('connection', handleConnection);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${process.env.SERVER_URL}`);
+// Server
+server.listen(PORT, () => {
+  console.log(`Server is running on ${SERVER_URL}`);
 });
