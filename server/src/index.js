9b3dcd9777dc7d5eca588ddb192803bd6d135fea
Colpno <gvinhh@gmail.com>
2022-12-25 16:28:23 +0700

    separate server file

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,11 @@
 import { config } from 'dotenv';
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
 
-import { handleConnection } from './controllers/socket/index.js';
-import { handleMiddleWares } from './middlewares/index.js';
-import { routeDivider } from './routes/index.js';
+import { server as app } from './app.js';
 
 config();
 
-const { PORT, SERVER_URL, CLIENT_URL, ADMIN_URL } = process.env;
-const SOCKET_SERVER_CONFIG = { cors: { origin: [CLIENT_URL, ADMIN_URL] } };
+const { PORT, SERVER_URL, BASE_URL } = process.env;
 
-const app = express();
-const server = http.createServer(app);
-const socketIO = new Server(server, SOCKET_SERVER_CONFIG);
-
-// Middle wares
-handleMiddleWares(app);
-
-// Routes
-routeDivider(app);
-
-// Socket
-socketIO.on('connection', handleConnection);
-
-// Server
-server.listen(PORT, () => {
-  console.log(`Server is running on ${SERVER_URL}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${SERVER_URL}${BASE_URL}`);
 });
