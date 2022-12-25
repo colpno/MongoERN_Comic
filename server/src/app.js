5e022f50aa6b56150b45317cdf64f05b40d8563b
Colpno <gvinhh@gmail.com>
2022-12-25 16:27:53 +0700

    change to mongodb

---
+++ server/src/app.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import moment from 'moment';
+
+import { socketConfig } from './config/socket.config.js';
+import { middlewares } from './middlewares/index.js';
+import routes from './routes/index.js';
+import connectMongoose from './helpers/connectMongoose.js';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, socketConfig);
+
+// Mongoose
+connectMongoose();
+
+app.use(middlewares);
+
+app.use(routes);
+
+// app.use((req, res, next) => {
+// res.io = io;
+//   next();
+// });
+
+// io.on('connection', (socket) => {
+//   console.log(`\n${moment().format('hh:mm:ss')} ${socket.id} connected`);
+
+//   socket.on('disconnect', () => {
+//     console.log(`${moment().format('hh:mm:ss')} ${socket.id} left`);
+//   });
+// });
+
+export { server };
