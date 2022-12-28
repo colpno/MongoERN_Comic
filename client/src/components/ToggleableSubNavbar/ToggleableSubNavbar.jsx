a61fcd7e8347a5167614d70310198282267d0903
Colpno <gvinhh@gmail.com>
2022-12-28 17:11:07 +0700

    remove hide component when click outside

--- client/src/components/ToggleableSubNavbar/ToggleableSubNavbar.jsx
+++ client/src/components/ToggleableSubNavbar/ToggleableSubNavbar.jsx
@@ -1,27 +1,17 @@
 import classNames from "classnames/bind";
 import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { SubNavigator } from "components";
-import { useClickOutSide } from "hooks";
-import { toggleHeaderNavBar } from "libs/redux/slices/globalSlice";
 import styles from "./ToggleableSubNavbar.module.scss";
 
 const cx = classNames.bind(styles);
 
 function ToggleableSubNavbar({ menu }) {
-  const dispatch = useDispatch();
   const isToggle = useSelector((state) => state.global.toggleHeaderNavBar);
 
-  const navbarRef = useClickOutSide(isToggle, () =>
-    dispatch(toggleHeaderNavBar(!isToggle))
-  );
-
   return (
-    <div
-      ref={navbarRef}
-      className={cx("toggleable-navbar", isToggle ? "active" : null)}
-    >
+    <div className={cx("toggleable-navbar", isToggle ? "active" : null)}>
       <SubNavigator menu={menu} />
     </div>
   );
