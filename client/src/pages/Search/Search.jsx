7030048686923a0f49d2d88c9066b1d5fbe5d97e
Colpno <gvinhh@gmail.com>
2022-12-28 21:55:30 +0700

    fix to run with mongo

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,8 +1,9 @@
 import classNames from "classnames/bind";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 import { CardList, FormWrapper } from "components";
+import { SEARCH_PAGE_TITLES_PER_PAGE } from "constants/paginate.constant";
 import { NoData, Pagination } from "features";
 import { usePagination } from "hooks";
 import { genreService, titleService } from "services";
@@ -13,16 +14,16 @@ import styles from "./styles/Search.module.scss";
 const cx = classNames.bind(styles);
 
 function Search() {
-  const TITLES_PER_PAGE = 35;
   const [genreArray, setGenreArray] = useState([]);
   const [titles, setTitles] = useState([]);
-  const { pagination, setPagination, setPaginationTotal } =
-    usePagination(TITLES_PER_PAGE);
+  const { pagination, setPagination, setPaginationTotal } = usePagination(
+    SEARCH_PAGE_TITLES_PER_PAGE
+  );
   const [initialValues, setInitialValues] = useState({
     genres: [],
     author: "",
     sort: "updatedAt",
-    _order: "desc",
+    order: "desc",
   });
   const [titleParams, setTitleParams] = useState({
     _sort: initialValues.sort,
@@ -31,28 +32,19 @@ function Search() {
     _limit: pagination.limit,
   });
 
-  const fetchData = (params = {}) => {
-    const genrePromise = genreService.getAll();
-    const titlePromise = titleService.getAll(params);
-
-    Promise.all([titlePromise, genrePromise])
-      .then(([titleResponse, genreResponse]) => {
-        setGenreArray(genreResponse);
-        setTitles(titleResponse.data);
-        setPaginationTotal(titleResponse.paginate.total);
-      })
-      .catch((error) => console.error(error));
-  };
-
-  const genreOptions = genreArray.map((genre) => ({
-    value: genre._id,
-    label: genre.name,
-  }));
+  const genreOptions = useMemo(
+    () =>
+      genreArray.map((genre) => ({
+        value: genre.name,
+        label: genre.name,
+      })),
+    [genreArray]
+  );
 
   const sortOptions = [
     { value: "updatedAt", label: "Ngày cập nhật" },
-    { value: "name", label: "Tên truyện" },
-    { value: "totalChapter", label: "Số chương" },
+    { value: "title", label: "Tên truyện" },
+    { value: "total_chapter", label: "Số chương" },
     { value: "like", label: "Lượt thích" },
     { value: "view", label: "Lượt xem" },
   ];
@@ -61,6 +53,16 @@ function Search() {
     { value: "asc", label: "Tăng dần" },
     { value: "desc", label: "Giảm dần" },
   ];
+
+  const fetchData = (params = {}) => {
+    titleService
+      .getAll(params)
+      .then((titleResponse) => {
+        setTitles(titleResponse.data);
+        setPaginationTotal(titleResponse.paginate.total);
+      })
+      .catch((error) => console.error(error));
+  };
 
   const checkChange = (values) => {
     const valueKeys = Object.keys(values);
@@ -90,13 +92,12 @@ function Search() {
         author_like: author,
         _limit: pagination.limit,
         _page: 1,
-        sort,
-        order,
+        _sort: sort,
+        _order: order,
       };
 
       if (checkedValues.genres) {
-        params.embed = "title_genre";
-        params.genres = genres;
+        params.genres_all = genres;
       }
 
       fetchData(params);
@@ -106,6 +107,19 @@ function Search() {
 
     setSubmitting(false);
   };
+
+  useEffect(() => {
+    const genrePromise = genreService.getAll();
+    const titlePromise = titleService.getAll(titleParams);
+
+    Promise.all([titlePromise, genrePromise])
+      .then(([titleResponse, genreResponse]) => {
+        setGenreArray(genreResponse.data);
+        setTitles(titleResponse.data);
+        setPaginationTotal(titleResponse.paginate.total);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   useEffect(() => {
     fetchData(titleParams);
