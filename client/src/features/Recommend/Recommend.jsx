90ed67eabdfba955b645015bbe471f79e551baa8
Colpno <gvinhh@gmail.com>
2022-12-29 09:26:15 +0700

    get the genres of title and fetch all titles with

--- client/src/features/Recommend/Recommend.jsx
+++ client/src/features/Recommend/Recommend.jsx
@@ -2,6 +2,8 @@ import classNames from "classnames/bind";
 import { memo, useEffect, useMemo, useState } from "react";
 import { Container } from "react-bootstrap";
 import { AiFillCaretDown } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import { Button, CardList } from "components";
 import { titleService } from "services";
@@ -10,40 +12,46 @@ import styles from "./Recommend.module.scss";
 const cx = classNames.bind(styles);
 
 function Recommend() {
+  const { titleId } = useParams();
   const [isExpand, setIsExpand] = useState(false);
   const [titles, setTitles] = useState([]);
+  const genresOfTitle = useSelector((state) => state.title.genresOfTitle);
   const unFoldList = useMemo(() => titles?.slice(6), [titles]);
   const foldList = useMemo(() => titles?.slice(0, 6), [titles]);
-
-  const fetchData = () => {
-    titleService
-      .getAll({ _limit: 18, _page: 1 })
-      .then((response) => setTitles(response.data))
-      .catch((error) => console.error(error));
-  };
 
   const showMoreCards = () => {
     setIsExpand((prev) => !prev);
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    titleService
+      .random(18, { genres_all: genresOfTitle })
+      .then((response) => {
+        const filtered = response.data.filter((title) => title._id !== titleId);
+        setTitles(filtered);
+      })
+      .catch((error) => console.error(error));
+  }, [genresOfTitle]);
 
   return (
-    <Container fluid="md" className={cx("recommend")}>
-      <div className={cx("head")}>
-        <h4 className={cx("title")}>Đề xuất</h4>
-        <Button text className={cx("more")} onClick={showMoreCards}>
-          <span>Xem thêm</span>
-          <AiFillCaretDown className={cx(isExpand ? "rotate" : "")} />
-        </Button>
-      </div>
-      <CardList wrap col={{ xs: 6, sm: 3, xl: 2 }} data={foldList} />
-      {isExpand && (
-        <CardList wrap col={{ xs: 6, sm: 3, xl: 2 }} data={unFoldList} />
-      )}
-    </Container>
+    <>
+      {titles.length > 0 ? (
+        <Container fluid="md" className={cx("recommend")}>
+          <div className={cx("head")}>
+            <h4 className={cx("title")}>Đề xuất</h4>
+            <Button text className={cx("more")} onClick={showMoreCards}>
+              <span>Xem thêm</span>
+              <AiFillCaretDown className={cx(isExpand ? "rotate" : "")} />
+            </Button>
+          </div>
+          <CardList wrap col={{ xs: 6, sm: 3, xl: 2 }} data={foldList} />
+          {isExpand && (
+            <CardList wrap col={{ xs: 6, sm: 3, xl: 2 }} data={unFoldList} />
+          )}
+        </Container>
+      ) : null}
+      {}
+    </>
   );
 }
 
