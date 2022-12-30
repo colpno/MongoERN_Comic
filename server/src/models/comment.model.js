ac32277978f7b3637b359f662d9586681741381d
Colpno <gvinhh@gmail.com>
2022-12-30 13:47:07 +0700

    reconstruct

--- server/src/models/comment.model.js
+++ server/src/models/comment.model.js
@@ -1,18 +1,31 @@
 import mongoose from 'mongoose';
+import { allowCommentPlaceList } from '../validations/commentAt.validation.js';
 
 const commentSchema = mongoose.Schema(
   {
-    user_id: { type: String, require: true },
+    author: {
+      _id: { type: String, require: true },
+      name: { type: String, require: true },
+      avatar: { type: String, require: true },
+    },
+    comment_at: {
+      type: String,
+      require: true,
+      validate: [allowCommentPlaceList, 'Nơi bình luận không hơp lệ'],
+    },
     text: { type: String, require: true },
-    reply_num: { type: Number, require: true, min: 0, default: 0 },
-    parent_id: { type: String, require: true },
+    comment_replies_num: { type: Number, require: true, min: 0, default: 0 },
+    slug: { type: String, require: true },
+    parent_slug: { type: String, require: true, default: '' },
+    full_slug: { type: String, require: true },
     hide: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
 commentSchema.pre('save', function (next) {
-  this.reply_num += 1;
+  this.slug += this._id;
+  this.full_slug += `${new Date().toISOString()}:${this._id}`;
 
   next();
 });
