6590f3d7868f8d900d95bc87c71014a65a7a923d
Colpno <gvinhh@gmail.com>
2023-01-01 22:48:42 +0700

    remove unnecessary

--- server/src/controllers/coinHistory.controller.js
+++ server/src/controllers/coinHistory.controller.js
@@ -1,4 +1,3 @@
-import createError from 'http-errors';
 import transformQueryParams from '../helpers/transformQueryParams.js';
 import { coinHistoryService } from '../services/index.js';
 
@@ -21,25 +20,6 @@ const coinHistoryController = {
       return res.status(200).json({
         ...response,
         code: 200,
-      });
-    } catch (error) {
-      return next(error);
-    }
-  },
-  add: async (req, res, next) => {
-    try {
-      const { id: userId } = req.userInfo;
-      const { paymentMethodId, amount } = req.body;
-
-      const response = await coinHistoryService.add(userId, paymentMethodId, amount);
-
-      if (!response) {
-        return next(createError(400, 'Không thể hoàn thành việc tạo giao dịch'));
-      }
-
-      return res.status(201).json({
-        code: 201,
-        data: response,
       });
     } catch (error) {
       return next(error);
