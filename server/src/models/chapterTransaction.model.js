84c3af6497d260e8922fbc8b8ebe73af8cb3a9d6
Colpno <gvinhh@gmail.com>
2023-01-01 13:04:44 +0700

    add title_id, rename some methods

--- server/src/models/chapterTransaction.model.js
+++ server/src/models/chapterTransaction.model.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 const chapterTransactionSchema = mongoose.Schema(
   {
     user_id: { type: String, require: true },
+    title_id: { type: String, require: true },
     chapter_id: { type: String, require: true },
     expiredAt: { type: Date },
     method: {
       type: String,
-      enum: ['coin', 'point', 'ticketForRenting', 'ticketForPurchasing'],
+      enum: ['coin', 'point', 'rent ticket', 'purchase ticket'],
       require: true,
     },
     cost: { type: Number, require: true },
