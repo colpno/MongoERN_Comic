fd802a9a516adf40278fe2faa39339d9ca930c12
Colpno <gvinhh@gmail.com>
2023-02-05 18:28:11 +0700

    change Table component

--- client/src/pages/MyTitle/MyTitle.jsx
+++ client/src/pages/MyTitle/MyTitle.jsx
@@ -5,12 +5,11 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { useSelector } from "react-redux";
 
 import { Button } from "components";
-import { NoData, Search } from "features";
-import { usePagination } from "hooks";
+import { NoData } from "features";
 import { titleService } from "services";
-import styles from "./styles/MyTitle.module.scss";
-import MyTitleContent from "./components/MyTitleContent";
+import MyTitleTable from "./components/MyTitleTable";
 import MyTitleHeader from "./components/MyTitleHeader";
+import styles from "./styles/MyTitle.module.scss";
 
 const cx = classNames.bind(styles);
 
@@ -24,39 +23,27 @@ function BtnCreate() {
 }
 
 function MyTitle() {
-  const TITLES_PER_PAGE = 50;
   const user = useSelector((state) => state.user.user);
   const searchText = useSelector((state) => state.global.searchText);
   const [titles, setTitles] = useState([]);
-  const { pagination, setPagination, setPaginationTotal } = usePagination(TITLES_PER_PAGE);
   const defaultTitleApiParams = {
     user_id: user._id,
     _sort: "_id",
     _order: "asc",
-    _page: pagination.page,
-    _limit: pagination.limit,
+    _embed: JSON.stringify([
+      { collection: "approved_status_id", fields: "-_id status color" },
+      { collection: "status_id", fields: "-_id status" },
+    ]),
+    _fields: "-__v -_guid -cover.cloud_public_id",
   };
   const [titleApiParams, setTitleApiParams] = useState(defaultTitleApiParams);
-  const [isDescSort, setIsDescSort] = useState(true);
   const hasData = titles.length > 0;
 
   const fetchData = (params) => {
     titleService
       .getAll(params)
-      .then((response) => {
-        setTitles(response.data);
-        setPaginationTotal(response.paginate.total);
-      })
+      .then((response) => setTitles(response.data))
       .catch((error) => console.error(error));
-  };
-
-  const handleSorting = (column) => {
-    setTitleApiParams((prev) => ({
-      ...prev,
-      _sort: column,
-      _order: isDescSort ? "desc" : "asc",
-    }));
-    setIsDescSort((prev) => !prev);
   };
 
   useEffect(() => {
@@ -75,24 +62,16 @@ function MyTitle() {
   return (
     <Container className={cx("my-title")}>
       <Row className={cx("my-title__header")}>
-        <Col xs={12} md="auto">
-          <MyTitleHeader cx={cx} totalTitle={pagination.total} />
+        <Col xs={6}>
+          <MyTitleHeader cx={cx} totalTitle={titles.length} />
         </Col>
-        <Col xs={6} md={4} lg={4} className="right">
-          <Search />
-        </Col>
-        <Col xs={6} md={3} lg={20} className="right">
+        <Col xs={6} className="right">
           {hasData && <BtnCreate />}
         </Col>
       </Row>
       {hasData ? (
         <Row>
-          <MyTitleContent
-            sorting={handleSorting}
-            titles={titles}
-            pagination={pagination}
-            setPagination={setPagination}
-          />
+          <MyTitleTable titles={titles} />
         </Row>
       ) : (
         <NoData>
--- client/src/pages/MyTitle/components/MyTitleTable.jsx
+++ client/src/pages/MyTitle/components/MyTitleTable.jsx
@@ -1,144 +1,189 @@
+import { GridActionsCellItem } from "@mui/x-data-grid";
 import classNames from "classnames/bind";
+import moment from "moment";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { memo } from "react";
 import { HiOutlinePencil } from "react-icons/hi";
 import { TbList } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 
-import { Button } from "components";
-import { approvedStatusService } from "services";
-import { formatTime } from "utils/convertTime";
+import { Button, Table } from "components";
+import { convertTimeLabel } from "constants/time.constant";
 import styles from "../styles/MyTitleTable.module.scss";
 
 const cx = classNames.bind(styles);
 
-const convertStatus = (status) => {
-  switch (status) {
-    case "vis":
-      return "Lưu hành";
-    case "inv":
-      return "Tạm ẩn";
-    default:
-      return "";
-  }
-};
+const headers = [
+  {
+    headerName: "Truyện",
+    field: "title",
+    width: 350,
+    minWidth: 350,
+    headerAlign: "center",
+    renderCell: ({ row }) => (
+      <>
+        <div className={cx("box-img")}>
+          <img src={row.cover.source} alt={row.title} />
+        </div>
+        <Button text to={`/comic/title/${row._id}`} className={cx("title")}>
+          {row.title}
+        </Button>
+      </>
+    ),
+  },
+  {
+    headerName: "Trạng thái",
+    field: "status_id",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+    valueGetter: ({ value }) => value.status,
+  },
+  {
+    headerName: "Duyệt",
+    field: "approved_status_id",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+    valueGetter: ({ value }) => value.status,
+    renderCell: ({ row, value }) => (
+      <span style={{ color: row.approved_status_id.color.hex, fontWeight: 700 }}>{value}</span>
+    ),
+  },
+  {
+    headerName: "Ngày ra chương",
+    field: "release_day",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+    renderCell: ({ value }) => <span>{convertTimeLabel(value, "shortLabel", "label")}</span>,
+  },
+  {
+    headerName: "Tác giả",
+    field: "author",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Thể loại",
+    field: "genres",
+    width: 140,
+    headerAlign: "center",
+    align: "center",
+    valueGetter: ({ value }) => value.join(", "),
+  },
+  {
+    headerName: "Tổng chương",
+    field: "total_chapter",
+    minWidth: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Coin",
+    field: "coin",
+    width: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Point",
+    field: "point",
+    width: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Lượt thích",
+    field: "like",
+    width: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Lượt xem",
+    field: "view",
+    width: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Số bình luận",
+    field: "comment_num",
+    width: 80,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    headerName: "Tóm tắt",
+    field: "summary",
+    width: 300,
+    headerAlign: "center",
+    sortable: false,
+  },
+  {
+    headerName: "Ngày tạo",
+    field: "createdAt",
+    width: 140,
+    align: "center",
+    headerAlign: "center",
+    renderCell: ({ value }) => (
+      <span className={cx("timestamp")}>{moment(value).format("DD.MM.YYYY")}</span>
+    ),
+  },
+  {
+    headerName: "Ngày sửa",
+    field: "updatedAt",
+    align: "center",
+    width: 140,
+    headerAlign: "center",
+    renderCell: ({ value }) => (
+      <span className={cx("timestamp")}>{moment(value).format("DD.MM.YYYY")}</span>
+    ),
+  },
+  {
+    field: "actions",
+    type: "actions",
+    getActions: ({ row }) => {
+      const navigate = useNavigate();
 
-function MyTitleTable({ data }) {
-  const [approvedStatuses, setApprovedStatuses] = useState([]);
-  const options = approvedStatuses.map((status) => {
-    return {
-      value: status._id,
-      label: status.status,
-    };
-  });
+      return [
+        <GridActionsCellItem
+          icon={<HiOutlinePencil />}
+          onClick={() => navigate(`update/${row._id}`)}
+          label="Update"
+        />,
+        <GridActionsCellItem
+          icon={<TbList />}
+          onClick={() => navigate(row._id)}
+          label="Show chapters"
+        />,
+      ];
+    },
+  },
+];
 
-  const getHexColor = (approvedStatusId) => {
-    const approvedStatus = approvedStatuses.find((status) => status._id === approvedStatusId);
-    return approvedStatus.color.hex;
-  };
-
-  const fetchData = () => {
-    approvedStatusService
-      .getAll()
-      .then((response) => {
-        setApprovedStatuses(response.data);
-      })
-      .catch((error) => console.error(error));
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+function MyTitleTable({ titles }) {
   return (
-    <>
-      {data.map((title, index) => {
-        const timeObj = formatTime(new Date(2022, 7, 16, 23, 16, 0, 0));
-        const approvedStatus =
-          options.length > 0 && options.find((option) => option.value === title.approved_status_id);
-
-        return (
-          <Row key={title._id} className={cx("my-title__container__content")}>
-            <Col xs={1}>
-              <span>{index}</span>
-            </Col>
-            <Col>
-              <div className={cx("box-img")}>
-                <img src={title.cover.source} alt={title.title} />
-              </div>
-            </Col>
-            <Col xs={3}>
-              <Button text to={`/comic/title/${title._id}`} className={cx("title")}>
-                {title.title}
-              </Button>
-            </Col>
-            <Col>
-              <span className={cx("status")}>{convertStatus(title.status)}</span>
-            </Col>
-            <Col>
-              {approvedStatuses.length > 0 ? (
-                <span
-                  className={cx("approved-status")}
-                  style={{ color: getHexColor(title.approved_status_id) }}
-                >
-                  {approvedStatus.label}
-                </span>
-              ) : null}
-            </Col>
-            <Col>
-              <span
-                className={cx("posted-time")}
-              >{`${timeObj.day}.${timeObj.month}.${timeObj.year}`}</span>
-            </Col>
-            <Col>
-              <span
-                className={cx("updated-time")}
-              >{`${timeObj.day}.${timeObj.month}.${timeObj.year}`}</span>
-            </Col>
-            <Col className="actions">
-              <Button
-                outline
-                grey
-                to={`update/${title._id}`}
-                className="action"
-                title="Chỉnh sửa truyện"
-              >
-                <HiOutlinePencil />
-              </Button>
-              <Button
-                outline
-                grey
-                to={`${title._id}`}
-                className="action"
-                title="Xem danh sách chương"
-              >
-                <TbList />
-              </Button>
-            </Col>
-          </Row>
-        );
-      })}
-    </>
+    <Table
+      headers={headers}
+      data={titles}
+      hasToolbar
+      autoHeight
+      rowHeight={100}
+      initialState={{
+        sorting: {
+          sortModel: [{ field: "approved_status_id", sort: "asc" }],
+        },
+        pinnedColumns: { left: ["title"], right: ["actions"] },
+      }}
+    />
   );
 }
 
 MyTitleTable.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      title: PropTypes.oneOfType([
-        PropTypes.string.isRequired,
-        PropTypes.arrayOf(
-          PropTypes.oneOfType([PropTypes.node.isRequired, PropTypes.string.isRequired]).isRequired
-        ).isRequired,
-      ]).isRequired,
-      cover: PropTypes.shape({
-        source: PropTypes.string.isRequired,
-      }).isRequired,
-      status: PropTypes.string.isRequired,
-      approved_status_id: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+  titles: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
-export default MyTitleTable;
+export default memo(MyTitleTable);
