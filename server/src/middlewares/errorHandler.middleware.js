3484a3be496db990990f44b084dc25ec52bd620b
Colpno <gvinhh@gmail.com>
2023-02-10 21:42:30 +0700

    response status if has

--- server/src/middlewares/errorHandler.middleware.js
+++ server/src/middlewares/errorHandler.middleware.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  res.status(500).json(err.message);
+  const { status, message } = err;
+
+  if (status && typeof status === 'number') return res.status(status).json(message);
+
+  res.status(500).json(message);
 };
 
 export default errorHandler;
