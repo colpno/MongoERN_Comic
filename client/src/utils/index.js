01d91bb4d08696675e93367d31c9499f7d9fae71
Colpno <gvinhh@gmail.com>
2023-02-10 22:49:16 +0700

    create some object prop getter

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -11,7 +11,8 @@ export {
   secondToHour,
   secondToMinute,
 } from "./convertTime";
+export { getObjectKeys, getObjectLength, getObjectValues } from "./getObjectProp";
 export { getReleaseDayOptions } from "./getReleaseDayOptions";
+export { default as handlePromiseAllSettled } from "./handlePromiseAllSettled";
 export { isEmpty } from "./isEmpty";
 export { roundNumByUnit, separateNumberDigit } from "./numberMethods";
-export { default as handlePromiseAllSettled } from "./handlePromiseAllSettled";
