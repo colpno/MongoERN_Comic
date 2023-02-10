935c44b16cf91106761a1a7888df8112960052d6
Colpno <gvinhh@gmail.com>
2023-02-10 19:18:41 +0700

    add field code and add unique to fields

--- server/src/models/approvedStatus.model.js
+++ server/src/models/approvedStatus.model.js
@@ -3,13 +3,13 @@ import { isHex } from '../validations/index.js';
 
 const approvedStatusSchema = mongoose.Schema(
   {
+    code: { type: String, require: true, unique: true },
     status: { type: String, require: true, unique: true },
     color: {
-      text: { type: String, require: true, unique: true },
+      text: { type: String, require: true },
       hex: {
         type: String,
         require: true,
-        unique: true,
         validate: [isHex, 'Mã màu phải là mã hex'],
       },
     },
