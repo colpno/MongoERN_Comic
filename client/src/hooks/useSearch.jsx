9873580c02313806d05d060a4f48df974d8202a1
Colpno <gvinhh@gmail.com>
2023-02-11 23:16:53 +0700

    change logic

--- client/src/hooks/useSearch.jsx
+++ client/src/hooks/useSearch.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 const checkContains = (text, match) => {
   return text.toLowerCase().includes(match.toLowerCase());
 };
 
-const handleHighlightText = (text, searchValue) => {
-  const highlightedText = text.split(new RegExp(`(${searchValue})`, "gi"));
+const highlightText = (text, match) => {
+  const highlightedText = text.split(new RegExp(`(${match})`, "gi"));
   return highlightedText.map((item, ind) => {
-    return item.toLowerCase() === searchValue.toLowerCase() ? (
+    return item.toLowerCase() === match.toLowerCase() ? (
       <mark key={ind}>{item}</mark>
     ) : (
       <span key={ind}>{item}</span>
@@ -13,108 +14,20 @@ const handleHighlightText = (text, searchValue) => {
   });
 };
 
-const handleSearchTitleNameAuthor = (searchedArray, searchValue, limit) => {
-  let index = 0;
+function useSearch(searchedArray, match, keys = []) {
+  const result = searchedArray.map((item) => {
+    const highlighted = {};
+    keys.forEach((key) => {
+      if (checkContains(item[key], match)) {
+        highlighted[key] = highlightText(item[key], match);
+      }
+    });
 
-  return searchedArray.reduce((titleArray, ttl) => {
-    const { title, author } = ttl;
+    const highlightedItem = { ...item, ...highlighted };
+    return highlightedItem;
+  });
 
-    if (
-      index !== limit &&
-      checkContains(title, searchValue) &&
-      checkContains(author, searchValue)
-    ) {
-      titleArray.push({
-        ...ttl,
-        title: handleHighlightText(title, searchValue),
-        author: handleHighlightText(author, searchValue),
-      });
-      index += 1;
-    }
-
-    return titleArray;
-  }, []);
-};
-
-const handleSearchTitleName = (searchedArray, searchValue, limit) => {
-  let index = 0;
-
-  return searchedArray.reduce((titleArray, ttl) => {
-    const { title } = ttl;
-
-    if (index !== limit && checkContains(title, searchValue)) {
-      titleArray.push({
-        ...ttl,
-        title: handleHighlightText(title, searchValue),
-      });
-      index += 1;
-    }
-
-    return titleArray;
-  }, []);
-};
-
-const handleSearchTitleAuthor = (searchedArray, searchValue, limit) => {
-  let index = 0;
-
-  return searchedArray.reduce((titleArray, title) => {
-    const { author } = title;
-
-    if (index !== limit && checkContains(author, searchValue)) {
-      titleArray.push({
-        ...title,
-        author: handleHighlightText(author, searchValue),
-      });
-      index += 1;
-    }
-
-    return titleArray;
-  }, []);
-};
-
-const checkDuplicate = (result = [], array = [], field = "") => {
-  const temp = [];
-
-  for (let i = 0; i < array.length; i++) {
-    const arr = array[i];
-
-    const isDuplicate = result.some((res) => arr[field] === res[field]);
-
-    if (!isDuplicate) temp.push(arr);
-  }
-
-  return temp;
-};
-
-function useSearch(searchedArray, searchValue, limit) {
-  const checkEmpty = (...array) => {
-    const tmp = array.map((arr) => arr.length === 0);
-    return tmp.some((item) => item === true);
-  };
-
-  const result = [];
-
-  const searchTitleNameAuthor = handleSearchTitleNameAuthor(
-    searchedArray,
-    searchValue,
-    limit
-  );
-
-  result.push(...searchTitleNameAuthor);
-
-  const searchTitleAuthor = checkEmpty(searchTitleNameAuthor)
-    ? handleSearchTitleAuthor(searchedArray, searchValue, limit)
-    : [];
-
-  result.push(...checkDuplicate(result, searchTitleAuthor, "_id"));
-
-  const searchTitleName = checkEmpty(searchTitleNameAuthor, searchTitleAuthor)
-    ? handleSearchTitleName(searchedArray, searchValue, limit)
-    : [];
-
-  result.push(...checkDuplicate(result, searchTitleName, "_id"));
-
-  return result.slice(0, limit);
+  return result;
 }
 
 export default useSearch;
