857a28c51f824d2568a457b67333ff839a7cc570
Colpno <gvinhh@gmail.com>
2023-02-11 18:06:43 +0700

    remove fetch top5

--- client/src/pages/Home/components/ComicSection.jsx
+++ client/src/pages/Home/components/ComicSection.jsx
@@ -6,6 +6,7 @@ import { useDispatch } from "react-redux";
 import { CardListWithTitle } from "components";
 import { setTop5Titles } from "libs/redux/slices/title.slice";
 import { genreService, titleService } from "services";
+import { sortArray } from "utils/arrayMethods";
 import styles from "../styles/ComicSection.module.scss";
 import ComicRankingSection from "./ComicRankingSection";
 
@@ -55,31 +56,16 @@ function ComicSection() {
   }, [titles.approvedTitles, genres]);
 
   useEffect(() => {
-    const top5QueryParams = {
-      _sort: "like",
-      _order: "desc",
-      _limit: 5,
-      _embed: JSON.stringify([
-        { collection: "approved_status_id", fields: "-_id code" },
-        { collection: "status_id", fields: "-_id code" },
-      ]),
-      _fields: "-__v -_guid -cover.cloud_public_id",
-    };
     const genresQueryParams = {
       _sort: "_id",
       _order: "asc",
       _limit: 4,
     };
 
-    const top5Promise = titleService.getAll(top5QueryParams, false);
-    const genresPromise = genreService.getAll(genresQueryParams);
-
-    Promise.all([top5Promise, genresPromise])
-      .then(([top5Response, genresResponse]) => {
-        const top5 = getConfirmedTitles(top5Response.data);
-
-        const allGenres = genresResponse.data.map((genre) => genre.name);
-
+    genreService
+      .getAll(genresQueryParams)
+      .then((genresResult) => {
+        const allGenres = genresResult.data.map((genre) => genre.name);
         const titlesQueryParams = {
           genres_in: allGenres,
           _embed: JSON.stringify([
@@ -88,13 +74,14 @@ function ComicSection() {
           ]),
           _fields: "-__v -_guid -cover.cloud_public_id",
         };
-        titleService.getAll(titlesQueryParams, false).then((titleResponse) => {
-          const approvedTitles = getConfirmedTitles(titleResponse.data);
+
+        titleService.getAll(titlesQueryParams, false).then((titleResult) => {
+          const approvedTitles = getConfirmedTitles(titleResult.data);
+          const top5 = sortArray(approvedTitles, "like", "desc").slice(0, 5);
 
           setTitles({ top5, approvedTitles });
+          setGenres(genresResult.data);
         });
-
-        setGenres(genresResponse.data);
       })
       .catch((error) => console.error(error));
   }, []);
