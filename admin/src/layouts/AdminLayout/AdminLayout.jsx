696acc5a0a852112ed9b84e0a5b2e182596004b3
Colpno <gvinhh@gmail.com>
2023-02-25 17:07:31 +0700

    remove global search

--- admin/src/layouts/AdminLayout/AdminLayout.jsx
+++ admin/src/layouts/AdminLayout/AdminLayout.jsx
@@ -1,8 +1,5 @@
 import classNames from "classnames/bind";
-import { setSearchText } from "libs/redux/slices/globalSlice";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 import AdminHeader from "./components/AdminHeader";
 import AdminSideBar from "./components/AdminSideBar";
@@ -11,12 +8,6 @@ import styles from "./styles/AdminLayout.module.scss";
 const cx = classNames.bind(styles);
 
 function AdminLayout({ children }) {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setSearchText(""));
-  });
-
   return (
     <div className={cx("admin-page")}>
       <AdminHeader cx={cx} />
