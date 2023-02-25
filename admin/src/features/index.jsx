f281cdb5cc143b5f95a6a9fd67af0a0acfa860ac
Colpno <gvinhh@gmail.com>
2023-02-25 17:04:22 +0700

    add pie chart

--- admin/src/features/index.jsx
+++ admin/src/features/index.jsx
@@ -7,6 +7,7 @@ export { default as LineChart } from "./LineChart/LineChart";
 export { default as Loading } from "./Loading/Loading";
 export { default as NoData } from "./NoData/NoData";
 export { default as Pagination } from "./Pagination/Pagination";
+export { default as PieChart } from "./PieChart/PieChart";
 export { default as Popup } from "./Popup/Popup";
 export { default as ProgressCircle } from "./ProgressCircle/ProgressCircle";
 export { default as Search } from "./Search/Search";
