19aba1f1f1d0cb0180830daba3d48ebbb0353acb
Colpno <gvinhh@gmail.com>
2023-02-25 17:08:53 +0700

    change props

--- admin/src/features/BarChart/BarChart.jsx
+++ admin/src/features/BarChart/BarChart.jsx
@@ -1,35 +1,49 @@
 import PropTypes from "prop-types";
 import { Bar } from "react-chartjs-2";
 
-function BarChart({ labels, datasets, width, height }) {
+function BarChart({ data, width, height, options }) {
   return (
     <div style={{ width, height }}>
-      <Bar data={{ labels, datasets }} />
+      <Bar data={data} options={options} />
     </div>
   );
 }
 
 BarChart.propTypes = {
-  labels: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  datasets: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string.isRequired,
-      data: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
-      backgroundColor: PropTypes.oneOfType([
-        PropTypes.string.isRequired,
-        PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-      ]),
-      borderColor: PropTypes.oneOfType([
-        PropTypes.string.isRequired,
-        PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-      ]),
-    }).isRequired
-  ).isRequired,
+  data: PropTypes.shape({
+    labels: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+    datasets: PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        data: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+        backgroundColor: PropTypes.oneOfType([
+          PropTypes.string.isRequired,
+          PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+        ]),
+        borderColor: PropTypes.oneOfType([
+          PropTypes.string.isRequired,
+          PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+        ]),
+      }).isRequired
+    ).isRequired,
+  }).isRequired,
+  options: PropTypes.shape({
+    plugins: PropTypes.shape({
+      legend: PropTypes.shape({
+        display: PropTypes.bool,
+      }),
+      tooltips: PropTypes.shape({
+        enabled: PropTypes.bool,
+      }),
+      indexAxis: PropTypes.oneOf(["y", undefined]),
+    }),
+  }),
   width: PropTypes.string,
   height: PropTypes.string,
 };
 
 BarChart.defaultProps = {
+  options: {},
   width: "100%",
   height: "100%",
 };
