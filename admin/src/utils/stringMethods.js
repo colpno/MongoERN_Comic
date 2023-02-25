c6af5aeedb78e8bd5833724078b3ea62f8a141d9
Colpno <gvinhh@gmail.com>
2023-02-25 17:08:00 +0700

    add replace all

--- admin/src/utils/stringMethods.js
+++ admin/src/utils/stringMethods.js
@@ -5,3 +5,7 @@ export function convertToString(number) {
 export function replaceAt(string, from, to, replacement) {
   return string.substring(0, from) + replacement + string.substring(to);
 }
+
+export function replaceAll(string, find, replace, flag = "g") {
+  return string.replace(new RegExp(find, flag), replace);
+}
