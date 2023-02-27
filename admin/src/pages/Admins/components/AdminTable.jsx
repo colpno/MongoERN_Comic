8ee79abe383613ea187981da6f33dfb010e22860
Colpno <gvinhh@gmail.com>
2023-02-27 12:33:04 +0700

    add crud to table

--- admin/src/pages/Admins/components/AdminTable.jsx
+++ admin/src/pages/Admins/components/AdminTable.jsx
@@ -1,21 +1,29 @@
-import { GridActionsCellItem } from "@mui/x-data-grid-pro";
 import classNames from "classnames/bind";
 import moment from "moment";
 import PropTypes from "prop-types";
-import { FaLock, FaTrash } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 import { MdCheck } from "react-icons/md";
 
+import { cat1 } from "assets/images";
 import { Table } from "components";
 import styles from "../styles/AdminTable.module.scss";
 
 const cx = classNames.bind(styles);
 
-function AdminTable({ admins, onDelete, onUpdate }) {
+function AdminTable({ admins, onDelete, onAdd, onUpdate }) {
   const initialState = {
     sorting: {
       sortModel: [{ field: "createdAt", sort: "desc" }],
     },
     pinnedColumns: { right: ["actions"] },
+  };
+
+  const initNewDataFields = {
+    username: "",
+    password: "",
+    role: "administrator",
+    email: "",
+    avatar: cat1,
   };
 
   return (
@@ -27,6 +35,7 @@ function AdminTable({ admins, onDelete, onUpdate }) {
           headerAlign: "center",
           flex: 1,
           minWidth: 200,
+          editable: true,
           renderCell: ({ row }) => {
             const { username, avatar } = row;
             return (
@@ -36,6 +45,20 @@ function AdminTable({ admins, onDelete, onUpdate }) {
               </div>
             );
           },
+        },
+        {
+          field: "password",
+          headerName: "Mật khẩu",
+          headerAlign: "center",
+          width: 140,
+          editable: true,
+        },
+        {
+          field: "email",
+          headerName: "Email",
+          headerAlign: "center",
+          width: 140,
+          editable: true,
         },
         {
           field: "isBanned",
@@ -91,34 +114,27 @@ function AdminTable({ admins, onDelete, onUpdate }) {
             <span className="timestamp">{moment(value).format("DD.MM.YYYY")}</span>
           ),
         },
-        {
-          field: "actions",
-          type: "actions",
-          width: 80,
-          getActions: ({ row }) => [
-            <GridActionsCellItem
-              size="large"
-              icon={<FaTrash />}
-              label="Delete"
-              onClick={() => onDelete(row._id)}
-            />,
-          ],
-        },
       ]}
       data={admins}
       hasToolbar
       height={700}
       rowHeight={100}
-      checkboxSelection
       initialState={initialState}
+      allowAdd
+      onAdd={onAdd}
+      initialNewDataFields={initNewDataFields}
+      allowEdit
+      onUpdate={onUpdate}
+      allowDelete
+      checkboxSelection
       onMultiDelete={onDelete}
-      onRowEditCommit={onUpdate}
     />
   );
 }
 
 AdminTable.propTypes = {
   admins: PropTypes.arrayOf(PropTypes.shape({}).isRequired).isRequired,
+  onAdd: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
   onUpdate: PropTypes.func.isRequired,
 };
