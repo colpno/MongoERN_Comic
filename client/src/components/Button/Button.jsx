372b289d2150588c057f181ed49153586bb8690b
Colpno <gvinhh@gmail.com>
2023-03-07 22:42:17 +0700

    remove unused, add HOC memo

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,6 +1,8 @@
 import classNames from "classnames/bind";
 import PropTypes from "prop-types";
+import { memo } from "react";
 import { Link } from "react-router-dom";
+
 import styles from "./Button.module.scss";
 
 const cx = classNames.bind(styles);
@@ -17,6 +19,7 @@ function Button(props) {
     target,
     type,
     title,
+    sx,
 
     primary,
     secondary,
@@ -24,36 +27,13 @@ function Button(props) {
     success,
     grey,
 
-    dark90,
-    dark80,
-    dark70,
-    dark60,
-    dark50,
-    dark40,
-    dark30,
-    dark20,
-    dark10,
-
-    light90,
-    light80,
-    light70,
-    light60,
-    light50,
-    light40,
-    light30,
-    light20,
-    light10,
-
-    marginTop1,
-
     wrapper,
     text,
+    xsmall,
     small,
     large,
     fullWidth,
     round,
-
-    ...others
   } = props;
   const classes = cx(
     wrapper || "btn",
@@ -67,39 +47,22 @@ function Button(props) {
       success,
       grey,
 
-      dark90,
-      dark80,
-      dark70,
-      dark60,
-      dark50,
-      dark40,
-      dark30,
-      dark20,
-      dark10,
-
-      light90,
-      light80,
-      light70,
-      light60,
-      light50,
-      light40,
-      light30,
-      light20,
-      light10,
-
-      marginTop1,
-
       wrapper,
       text,
       round,
       small,
+      xsmall,
       large,
     },
     className
   );
-  const attributes = { onClick, type, title, ...others };
-  attributes.target = target || null;
-  attributes.type = type || "button";
+  const attributes = {
+    onClick,
+    target: target || null,
+    type: type || "button",
+    title: title || null,
+    sx: sx || null,
+  };
 
   let Component = "button";
   if (to) {
@@ -129,6 +92,7 @@ Button.propTypes = {
   target: PropTypes.string,
   type: PropTypes.string,
   title: PropTypes.string,
+  sx: PropTypes.string,
 
   primary: PropTypes.bool,
   secondary: PropTypes.bool,
@@ -136,31 +100,10 @@ Button.propTypes = {
   success: PropTypes.bool,
   grey: PropTypes.bool,
 
-  dark90: PropTypes.bool,
-  dark80: PropTypes.bool,
-  dark70: PropTypes.bool,
-  dark60: PropTypes.bool,
-  dark50: PropTypes.bool,
-  dark40: PropTypes.bool,
-  dark30: PropTypes.bool,
-  dark20: PropTypes.bool,
-  dark10: PropTypes.bool,
-
-  light90: PropTypes.bool,
-  light80: PropTypes.bool,
-  light70: PropTypes.bool,
-  light60: PropTypes.bool,
-  light50: PropTypes.bool,
-  light40: PropTypes.bool,
-  light30: PropTypes.bool,
-  light20: PropTypes.bool,
-  light10: PropTypes.bool,
-
-  marginTop1: PropTypes.bool,
-
   wrapper: PropTypes.bool,
   text: PropTypes.bool,
   small: PropTypes.bool,
+  xsmall: PropTypes.bool,
   large: PropTypes.bool,
   fullWidth: PropTypes.bool,
   round: PropTypes.bool,
@@ -175,6 +118,7 @@ Button.defaultProps = {
   target: "",
   type: "",
   title: "",
+  sx: "",
 
   primary: false,
   secondary: false,
@@ -182,34 +126,13 @@ Button.defaultProps = {
   success: false,
   grey: false,
 
-  dark90: false,
-  dark80: false,
-  dark70: false,
-  dark60: false,
-  dark50: false,
-  dark40: false,
-  dark30: false,
-  dark20: false,
-  dark10: false,
-
-  light90: false,
-  light80: false,
-  light70: false,
-  light60: false,
-  light50: false,
-  light40: false,
-  light30: false,
-  light20: false,
-  light10: false,
-
-  marginTop1: false,
-
   wrapper: false,
   text: false,
   small: false,
+  xsmall: false,
   large: false,
   fullWidth: false,
   round: false,
 };
 
-export default Button;
+export default memo(Button);
