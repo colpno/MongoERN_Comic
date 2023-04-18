eadf4b0a4e1ae790e83938b854d28c4b391e82ab
Colpno <gvinhh@gmail.com>
2023-04-18 15:31:49 +0700

    add dark mode switcher

--- client/src/layouts/ReadingLayout/components/ReadingTools.jsx
+++ client/src/layouts/ReadingLayout/components/ReadingTools.jsx
@@ -1,22 +1,20 @@
-import { useLike } from "hooks";
 import PropTypes from "prop-types";
 import { memo } from "react";
-
-import { FaLightbulb, FaShareAlt } from "react-icons/fa";
+import { FaShareAlt } from "react-icons/fa";
 import { ImHeart } from "react-icons/im";
 import { MdFitScreen } from "react-icons/md";
 import { useParams } from "react-router-dom";
 
-function ReadingTools({ cx, darkTheme, handleChangeTheme }) {
+import { DarkModeToggle } from "features";
+import { useLike } from "hooks";
+
+function ReadingTools({ cx }) {
   const { chapterId } = useParams();
   const { isLike, handleLike } = useLike(chapterId);
 
   return (
     <div className={cx("reading-header__tools")}>
-      <FaLightbulb
-        className={cx("reading-header__tools__theme", darkTheme ? "active" : "")}
-        onClick={handleChangeTheme}
-      />
+      <DarkModeToggle />
       <ImHeart
         className={cx("reading-header__tools__like", isLike ? "active" : "")}
         onClick={handleLike}
@@ -29,8 +27,6 @@ function ReadingTools({ cx, darkTheme, handleChangeTheme }) {
 
 ReadingTools.propTypes = {
   cx: PropTypes.func.isRequired,
-  darkTheme: PropTypes.bool.isRequired,
-  handleChangeTheme: PropTypes.func.isRequired,
 };
 
 export default memo(ReadingTools);
