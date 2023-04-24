32e38bbbdff5c0c2a60dc8acae264e4901b11fac
Colpno <gvinhh@gmail.com>
2023-04-24 10:20:18 +0700

    add new method

--- admin/src/utils/index.js
+++ admin/src/utils/index.js
@@ -1,6 +1,7 @@
-export { createArrayFromTo, makeUnique } from "./arrayMethods";
+export { createArrayFromTo, makeUnique, moveArray } from "./arrayMethods";
 export { getObjectKeys, getObjectLength, getObjectValues } from "./getObjectProp";
 export { getReleaseDayOptions } from "./getReleaseDayOptions";
 export { default as handlePromiseAllSettled } from "./handlePromiseAllSettled";
 export { isEmpty } from "./isEmpty";
 export { roundNumByUnit, separateNumberDigit } from "./numberMethods";
+export { replaceAll } from "./stringMethods";
