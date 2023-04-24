fb33838fb94eba0579bf449d739b248fd4ee9a69
Colpno <gvinhh@gmail.com>
2023-04-24 10:10:53 +0700

    add className prop

--- client/src/components/HeadTitleMark/HeadTitleMark.jsx
+++ client/src/components/HeadTitleMark/HeadTitleMark.jsx
@@ -5,12 +5,17 @@ import styles from "./HeadTitleMark.module.scss";
 
 const cx = classNames.bind(styles);
 
-function HeadTitleMark({ children }) {
-  return <div className={cx("mark")}>{children}</div>;
+function HeadTitleMark({ children, classname }) {
+  return <div className={cx("mark", classname)}>{children}</div>;
 }
 
 HeadTitleMark.propTypes = {
   children: PropTypes.node.isRequired,
+  classname: PropTypes.string,
+};
+
+HeadTitleMark.defaultProps = {
+  classname: "",
 };
 
 export default HeadTitleMark;
