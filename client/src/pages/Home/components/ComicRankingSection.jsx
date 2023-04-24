7970c7bef31c155b8b632a3f55efabfe992ac8af
Colpno <gvinhh@gmail.com>
2023-04-24 10:13:16 +0700

    animate the UI

--- client/src/pages/Home/components/ComicRankingSection.jsx
+++ client/src/pages/Home/components/ComicRankingSection.jsx
@@ -1,22 +1,31 @@
-import PropTypes from "prop-types";
-import { memo } from "react";
-import { Container } from "react-bootstrap";
 import classNames from "classnames/bind";
+import PropTypes from "prop-types";
+import { memo, useState } from "react";
+import { Container } from "react-bootstrap";
 
-import { Button } from "components";
+import { Button, HeadTitleMark } from "components";
+import { useObserver } from "hooks";
 import Top5 from "pages/Ranking/components/Top5";
 import styles from "../styles/ComicRankingSection.module.scss";
 
 const cx = classNames.bind(styles);
 
 function ComicRankingSection({ top5 }) {
+  const [isActivated, setIsActivated] = useState(false);
+  const { setLastElementRef } = useObserver(
+    () => setIsActivated(true),
+    () => setIsActivated(false)
+  );
+
   return (
-    <section className={cx("ranking__wrapper")}>
-      <Container className={cx("container", "ranking")}>
-        <header className={cx("head")}>
-          <h3 className={cx("title")}>Ranking</h3>
+    <section className={cx("ranking-section")}>
+      <Container>
+        <header className={cx("head", isActivated && "active")} ref={setLastElementRef}>
+          <HeadTitleMark classname={cx("title")}>
+            <h3>Ranking</h3>
+          </HeadTitleMark>
           <Button text to="/comic/ranking" className={cx("btn--more")}>
-            Xem them
+            Xem thêm
           </Button>
         </header>
         <Top5 titles={top5} />
