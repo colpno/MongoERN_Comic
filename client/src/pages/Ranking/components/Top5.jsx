da6e8b60a5aaf70a1da75dc0f9b657c1e7a2d7f0
Colpno <gvinhh@gmail.com>
2023-04-24 10:14:55 +0700

    animate the UI

--- client/src/pages/Ranking/components/Top5.jsx
+++ client/src/pages/Ranking/components/Top5.jsx
@@ -1,31 +1,43 @@
 import classNames from "classnames/bind";
-import { memo } from "react";
 import PropTypes from "prop-types";
+import { memo, useMemo, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
+import { useObserver } from "hooks";
 import styles from "../styles/Top5.module.scss";
 import RankingFigure from "./RankingFigure";
 
 const cx = classNames.bind(styles);
 
 function Top5({ titles }) {
-  const top2 = titles.slice(0, 2);
-  const top3 = titles.slice(2, 3);
-  const top5 = titles.slice(3, 5);
+  const [isActivated, setIsActivated] = useState(false);
+  const { setLastElementRef } = useObserver(
+    () => setIsActivated(true),
+    () => setIsActivated(false)
+  );
+
+  const { top2, top3, top5 } = useMemo(
+    () => ({
+      top2: titles.slice(0, 2),
+      top3: titles[2],
+      top5: titles.slice(3, 5),
+    }),
+    [titles]
+  );
 
   return (
-    <div className={cx("top5-wrapper")}>
-      <Row className={cx("top5-wrapper__cards")}>
-        {top2.map((data, index) => {
+    <div className={cx("top5-wrapper")} ref={setLastElementRef}>
+      <Row className={cx("top-5-list")}>
+        {top2.map((top, index) => {
           return (
             <Col
               md={12}
               lg={6}
               key={index}
-              className={cx("top5-wrapper__cards__card", "top-2")}
+              className={cx("top", isActivated && "active", `top-${index + 1}`)}
             >
               <RankingFigure
-                data={data}
+                data={top}
                 rank={index + 1}
                 classN={{
                   bookmark: cx("icon-bookmark"),
@@ -37,34 +49,29 @@ function Top5({ titles }) {
             </Col>
           );
         })}
-        {top3.map((data, index) => {
-          return (
-            <Col
-              md={{ span: 8, offset: 2 }}
-              lg={{ span: 4, offset: 0 }}
-              key={index}
-              className={cx("top5-wrapper__cards__card", "top-3")}
-            >
-              <RankingFigure
-                data={data}
-                rank={index + 3}
-                classN={{
-                  bookmark: cx("icon-bookmark"),
-                }}
-              />
-            </Col>
-          );
-        })}
-        {top5.map((data, index) => {
+        <Col
+          md={{ span: 8, offset: 2 }}
+          lg={{ span: 4, offset: 0 }}
+          className={cx("top", isActivated && "active", `top-3`)}
+        >
+          <RankingFigure
+            data={top3}
+            rank={3}
+            classN={{
+              bookmark: cx("icon-bookmark"),
+            }}
+          />
+        </Col>
+        {top5.map((top, index) => {
           return (
             <Col
               sm={12}
               md={6}
               lg={4}
-              key={index}
-              className={cx("top5-wrapper__cards__card", "top-4-5")}
+              key={index + 4}
+              className={cx("top", isActivated && "active", `top-${index + 4}`)}
             >
-              <RankingFigure data={data} rank={index + 4} />
+              <RankingFigure data={top} rank={index + 4} />
             </Col>
           );
         })}
