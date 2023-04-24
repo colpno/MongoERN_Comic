371053249e2fa1877881bd030c0df882936bc493
Colpno <gvinhh@gmail.com>
2023-04-24 10:17:35 +0700

    re-export useObserver and remove deleted hooks

--- client/src/hooks/index.jsx
+++ client/src/hooks/index.jsx
@@ -1,15 +1,13 @@
-export { default as useAdd } from "./useAdd";
 export { default as useClickOutSide } from "./useClickOutSide";
 export { default as useDebounce } from "./useDebounce";
-export { default as useDelete } from "./useDelete";
 export { default as useFollow } from "./useFollow";
 export { default as useInfinitePagination } from "./useInfinitePagination";
 export { default as useLike } from "./useLike";
 export { default as useLogout } from "./useLogout";
 export { default as useMultipleForms } from "./useMultipleForms";
+export { default as useObserver } from "./useObserver";
 export { default as usePagination } from "./usePagination";
 export { default as usePopup } from "./usePopup";
 export { default as usePreviewImage } from "./usePreviewImage";
 export { default as useSearch } from "./useSearch";
 export { default as useToast } from "./useToast";
-export { default as useUpdate } from "./useUpdate";
