d7a5587e3bf80236984c4d8d4ca7215b6d02de43
Colpno <gvinhh@gmail.com>
2023-04-24 10:13:51 +0700

    add Toast, separators

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,13 +4,15 @@ import { Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 import { BannerSlider } from "features";
-import styles from "./styles/Home.module.scss";
+import { useToast } from "hooks";
 import { ComicSection, Notification } from "./components";
+import styles from "./styles/Home.module.scss";
 
 const cx = classNames.bind(styles);
 
 function Home() {
   const top5Titles = useSelector((state) => state.title.top5);
+  const { Toast, options } = useToast();
 
   const banners = useMemo(
     () =>
@@ -24,20 +26,24 @@ function Home() {
   return (
     <main className={cx("home")}>
       <BannerSlider images={banners} />
+      <div className={cx("separator")} />
       <ComicSection />
       <Notification />
+      <div className={cx("separator")} />
       <Container>
         <h1 className={cx("title")}>Novels</h1>
         <section className={cx("novel-action")} />
         <section className={cx("novel-horror")} />
         <section className={cx("novel-ranking")} />
       </Container>
+      <div className={cx("separator")} />
       <Container>
         <h1 className={cx("title")}>Anime</h1>
         <section className={cx("anime-action")} />
         <section className={cx("anime-horror")} />
         <section className={cx("anime-ranking")} />
       </Container>
+      <Toast {...options} />
     </main>
   );
 }
