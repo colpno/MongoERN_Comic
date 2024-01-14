065be15c529c2cdd2a9e9893388913b495f2b040
Colpno <gvinhh@gmail.com>
2024-01-14 16:23:45 +0700

    add logic and complete

--- client/src/pages/NoticeList/NoticeList.jsx
+++ client/src/pages/NoticeList/NoticeList.jsx
@@ -1,35 +1,36 @@
-/* eslint-disable no-unused-vars */
 import classNames from "classnames/bind";
+import { memo } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
+import { useGetAllNotificationsQuery } from "api/notification.api.js";
 import { Button } from "components";
-import styles from "./styles/NoticeList.module.scss";
 import NoticeFigure from "./components/NoticeFigure";
+import styles from "./styles/NoticeList.module.scss";
 
 const cx = classNames.bind(styles);
 
 function NoticeList() {
-  // const notices = NoticeArray();
+  const { data: notifications } = useGetAllNotificationsQuery({
+    _sort: "updatedAt",
+    _order: -1,
+  });
 
   return (
     <Container className={cx("notice-container")}>
       <Row>
-        {/* {notices.map((notice) => {
-          return (
-            <Col
-              key={notice._id}
-              md={4}
-              className={cx("notice-container__notice")}
-            >
-              <Button wrapper to={`/notice/${notice._id}`}>
-                <NoticeFigure data={notice} />
-              </Button>
-            </Col>
-          );
-        })} */}
+        {notifications?.length > 0 &&
+          notifications.map((notice) => {
+            return (
+              <Col key={notice._id} md={4} className={cx("notice-container__notice")}>
+                <Button wrapper to={`/notice/${notice._id}`}>
+                  <NoticeFigure data={notice} />
+                </Button>
+              </Col>
+            );
+          })}
       </Row>
     </Container>
   );
 }
 
-export default NoticeList;
+export default memo(NoticeList);
--- client/src/pages/NoticeList/components/NoticeFigure.jsx
+++ client/src/pages/NoticeList/components/NoticeFigure.jsx
@@ -1,21 +1,26 @@
 import classNames from "classnames/bind";
 import PropTypes from "prop-types";
 
+import moment from "moment";
+import { AiOutlineClockCircle } from "react-icons/ai";
 import styles from "../styles/NoticeFigure.module.scss";
 
 const cx = classNames.bind(styles);
 
 function NoticeFigure({ data }) {
-  const { title, cover, content } = data;
+  const { title, cover, updatedAt } = data;
 
   return (
     <figure className={cx("notice-figure")}>
       <div className={cx("box-img")}>
-        <img src={cover} alt={title} />
+        <img src={cover.source} alt={title} />
       </div>
       <figcaption className={cx("notice-figure__content")}>
+        <p className={cx("update-time")}>
+          <AiOutlineClockCircle />
+          <small>{moment(updatedAt).format("DD.MM.YYYY HH:mm")}</small>
+        </p>
         <h5 className={cx("title")}>{title}</h5>
-        <small className={cx("content")}>{content}</small>
       </figcaption>
     </figure>
   );
@@ -24,8 +29,10 @@ function NoticeFigure({ data }) {
 NoticeFigure.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    cover: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
+    cover: PropTypes.shape({
+      source: PropTypes.string.isRequired,
+    }),
+    updatedAt: PropTypes.string.isRequired,
   }).isRequired,
 };
 
