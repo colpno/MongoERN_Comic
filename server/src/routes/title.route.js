a567d021e4232017781e15dd40eb57a20c9e78a3
Colpno <gvinhh@gmail.com>
2024-01-16 02:19:32 +0700

    fixed route priority

--- server/src/routes/title.route.js
+++ server/src/routes/title.route.js
@@ -6,11 +6,11 @@ const route = express.Router();
 
 route.get('/', titleController.getAll);
 route.get('/random', titleController.random);
-route.get('/:id', titleController.getOne);
 route.get('/owned', isAuthenticated, titleController.getAll);
 route.get('/owned/:id', isAuthenticated, titleController.getOne);
 route.post('/create', isAuthenticated, titleController.add);
 route.put('/update/:id', isAuthenticated, titleController.update);
 route.delete('/delete/:id', isAuthenticated, titleController.delete);
+route.get('/:id', titleController.getOne);
 
 export default route;
