8c39fade61d6711a2c494f420a1c83ad5b66fd94
Colpno <gvinhh@gmail.com>
2024-01-19 16:17:31 +0700

    title no more required

--- client/src/validations/createChapterForm.validation.js
+++ client/src/validations/createChapterForm.validation.js
@@ -2,9 +2,7 @@ import * as Yup from "yup";
 
 export const createChapterFormValidation = Yup.object({
   order: Yup.number(),
-  title: Yup.string()
-    .max(255, "Giới hạn độ dài là 255 ký tự.")
-    .required("Tiêu đề không được để trống."),
+  title: Yup.string().max(255, "Giới hạn độ dài là 255 ký tự."),
   cost: Yup.string().required("Chương tối thiểu là miễn phí"),
   cover: Yup.string().required("Ảnh bìa không được để trống."),
   contents: Yup.array().of(Yup.string()).min(1, "Nội dung không được để trống"),
