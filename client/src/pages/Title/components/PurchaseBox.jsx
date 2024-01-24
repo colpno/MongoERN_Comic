cd18243b436f8ac7c201d5c92d620ff6d5fc99fe
Colpno <gvinhh@gmail.com>
2024-01-24 17:09:39 +0700

    improve the logic

--- client/src/pages/Title/components/PurchaseBox.jsx
+++ client/src/pages/Title/components/PurchaseBox.jsx
@@ -1,6 +1,5 @@
 import classNames from "classnames/bind";
 import PropTypes from "prop-types";
-import { useState } from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 
 import { Button } from "components";
@@ -10,43 +9,31 @@ import styles from "../styles/PurchaseBox.module.scss";
 const cx = classNames.bind(styles);
 
 function PurchaseBox({ chapter, payments, handleSubmit, handleClose }) {
-  const [value, setValue] = useState(payments[0]);
-
   return (
     <Dialog handleClickOutside={handleClose}>
       <div className={cx("purchase-box")}>
         <AiFillCloseCircle className={cx("close-button")} onClick={handleClose} />
         <div className={cx("image-box")}>
-          <h4 className={cx("title")}>{chapter.title}</h4>
+          <h4 className={cx("title")}>
+            Chương {chapter.order}
+            {chapter.title ? `: ${chapter.title}` : ""}
+          </h4>
           <img src={chapter.cover.source} alt="title cover" />
         </div>
         <div className={cx("payment-box")}>
           <div className={cx("choices")}>
-            {payments.map((payment, index) => {
-              const isActive = payment.method === value.method;
-
-              return (
-                <Button outline={!isActive} primary={isActive} className={cx("button")} key={index}>
-                  <span>{payment.amount}</span>
-                  <img src={payment.icon} alt="payment icon" />
-                  <input
-                    type="radio"
-                    value={value}
-                    onClick={() => setValue(payment)}
-                    className={cx("radio")}
-                  />
-                </Button>
-              );
-            })}
+            {payments.map((payment, index) => (
+              <Button
+                primary
+                className={cx("button")}
+                key={index}
+                onClick={() => handleSubmit(payment, chapter)}
+              >
+                <span>{payment.amount}</span>
+                <img src={payment.icon} alt="payment icon" />
+              </Button>
+            ))}
           </div>
-          <Button
-            primary
-            round
-            className={cx("submit")}
-            onClick={() => handleSubmit(value, chapter)}
-          >
-            Mua/Thuê
-          </Button>
         </div>
       </div>
     </Dialog>
@@ -59,6 +46,7 @@ PurchaseBox.propTypes = {
       source: PropTypes.string.isRequired,
     }).isRequired,
     title: PropTypes.string.isRequired,
+    order: PropTypes.number.isRequired,
   }).isRequired,
   payments: PropTypes.arrayOf(
     PropTypes.shape({
