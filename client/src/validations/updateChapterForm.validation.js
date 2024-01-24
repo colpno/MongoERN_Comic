992e01079a6b55374373140caa345aa37d276d7c
Colpno <gvinhh@gmail.com>
2024-01-24 17:10:26 +0700

    remove required on field title

--- client/src/validations/updateChapterForm.validation.js
+++ client/src/validations/updateChapterForm.validation.js
@@ -2,9 +2,7 @@ import * as Yup from "yup";
 
 export const updateChapterFormValidation = Yup.object({
   order: Yup.number(),
-  title: Yup.string()
-    .max(255, "Giới hạn độ dài là 255 ký tự.")
-    .required("Truyện cần phải có tiêu đề."),
+  title: Yup.string().max(255, "Giới hạn độ dài là 255 ký tự."),
   cost: Yup.string().required("Truyện tối thiểu là miễn phí"),
   cover: Yup.string().required("Truyện cần phải có ảnh bìa mặc định."),
   contents: Yup.array().of(Yup.string()).min(1, "Cần tối thiểu 1 nội dung"),
