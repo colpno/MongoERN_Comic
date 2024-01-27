fc7599130cd0129fe2ed859f2842469e1bdcabf8
Colpno <gvinhh@gmail.com>
2024-01-27 20:55:58 +0700

    disallow to click on chapter if not buy

--- client/src/pages/Reading/components/ReadingPagination.jsx
+++ client/src/pages/Reading/components/ReadingPagination.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames/bind";
+import { IoIosLock } from "react-icons/io";
 import PropTypes from "prop-types";
 import { memo } from "react";
 import { useParams } from "react-router-dom";
@@ -11,8 +12,8 @@ import styles from "../assets/styles/ReadingPagination.module.scss";
 
 const cx = classNames.bind(styles);
 
-function ReadingPagination({ chapters }) {
-  const { titleId, chapterId } = useParams();
+function ReadingPagination({ chapters, boughtChapter }) {
+  const { titleId, page } = useParams();
 
   return (
     <div className={cx("reading-page__pagination")}>
@@ -20,7 +21,6 @@ function ReadingPagination({ chapters }) {
         <Slider
           outsideNavigation
           grabCursor
-          centeredSlides
           preloadImages={false}
           lazy
           spaceBetween={20}
@@ -38,17 +38,25 @@ function ReadingPagination({ chapters }) {
           }}
         >
           {chapters.map((chapter) => {
+            const isBought = boughtChapter.some((bought) => bought._id === chapter._id);
+
             return (
               <SwiperSlide key={chapter._id}>
                 <Button
                   wrapper
-                  to={`/comic/title/${titleId}/${chapter._id}`}
-                  className={cx("slide-wrapper", chapterId === chapter._id ? "active" : "")}
+                  to={isBought ? `/comic/title/${titleId}/${chapter.order}` : undefined}
+                  className={cx("slide-wrapper", page === `${chapter.order}` ? "active" : "")}
                 >
                   <div className={cx("box-img")}>
-                    <img src={chapter.cover.source} alt={chapter.title} />
+                    <img
+                      src={chapter.cover.source}
+                      alt={`Chương ${chapter.order}${chapter.title ? `: ${chapter.title}` : ""}`}
+                    />
                   </div>
-                  <span className={cx("content")}>{chapter.title}</span>
+                  <span className={cx("content")}>
+                    {!isBought && <IoIosLock className={cx("lock-icon")} />}
+                    {`Chương ${chapter.order}${chapter.title ? `: ${chapter.title}` : ""}`}
+                  </span>
                 </Button>
               </SwiperSlide>
             );
@@ -68,6 +76,12 @@ ReadingPagination.propTypes = {
         source: PropTypes.string.isRequired,
       }).isRequired,
       title: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+
+  boughtChapter: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
 };
