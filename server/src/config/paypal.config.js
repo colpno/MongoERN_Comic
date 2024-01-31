33f99626b96f71ac65cbd9122a7a76f5814cb154
Colpno <gvinhh@gmail.com>
2024-02-01 02:11:36 +0700

    remove paypal-rest-sdk

--- server/src/config/paypal.config.js
+++ server/src/config/paypal.config.js
@@ -2,15 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PAYPAL_CLIENT_ID, PAYPAL_SECRET } = process.env;
-
-const paypalConfig = {
-  mode: 'sandbox', // sandbox or live
-  client_id: PAYPAL_CLIENT_ID,
-  client_secret: PAYPAL_SECRET,
-};
-
-export default paypalConfig;
-
-// email: sb-4iedz24663946@personal.example.com
-// pass:  E<9Uy!j9
+export const { PAYPAL_CLIENT_ID, PAYPAL_SECRET } = process.env;
+export const PAYPAL_URL = 'https://api-m.sandbox.paypal.com';
