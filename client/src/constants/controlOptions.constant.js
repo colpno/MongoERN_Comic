1f82d038d4a0d7cef1cfbd01c9a8758070097e31
Colpno <gvinhh@gmail.com>
2024-01-31 13:39:34 +0700

    create dollar-coin converter

--- client/src/constants/controlOptions.constant.js
+++ client/src/constants/controlOptions.constant.js
@@ -1,3 +1,5 @@
+import { emitToast } from "features/Toast.jsx";
+
 export const coinOptions = [
   { value: "1.00", label: "10 coin = $1" },
   { value: "2.00", label: "20 coin = $2" },
@@ -5,3 +7,14 @@ export const coinOptions = [
   { value: "10.00", label: "100 coin = $10" },
   { value: "20.00", label: "200 coin = $20" },
 ];
+
+export const dollarToCoin = (dollar = "") => {
+  try {
+    const optionLabel = coinOptions.find((option) => option.value === dollar).label;
+    const coin = Number.parseInt(optionLabel.split(" ")[0], 10);
+    return coin;
+  } catch (error) {
+    emitToast(error, "error");
+    return undefined;
+  }
+};
