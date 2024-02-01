a0d8bdd0327cc59fde8f3e00f7d9cc59a816c6d0
Colpno <gvinhh@gmail.com>
2024-02-02 02:37:17 +0700

    refactor code

--- client/src/pages/Statistic/Statistic.jsx
+++ client/src/pages/Statistic/Statistic.jsx
@@ -1,22 +1,29 @@
 import classNames from "classnames/bind";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Container } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 
 import { TabsContainer } from "components";
-import IncomeStat from "./components/IncomeStat";
-import LikeViewStat from "./components/LikeViewStat";
+import IncomeStat from "./IncomeStat/IncomeStat";
+import LikeViewStat from "./LikeViewStat/LikeViewStat";
 import styles from "./styles/Statistic.module.scss";
 
 const cx = classNames.bind(styles);
 
+const menu = [
+  {
+    tab: "like-view",
+    href: "?tab=like-view",
+    label: "Lượt xem - Lượt thích",
+    Component: LikeViewStat,
+  },
+  { tab: "income", href: "?tab=income", label: "Thu nhập cá nhân", Component: IncomeStat },
+];
+
 function Statistic() {
   const [searchParams] = useSearchParams();
   let queryTab = searchParams.get("tab") || "";
-  const menu = [
-    { tab: "like-view", href: "?tab=like-view", label: "Lượt xem - Lượt thích" },
-    { tab: "income", href: "?tab=income", label: "Thu nhập cá nhân" },
-  ];
+  const { Component } = useMemo(() => menu.find((item) => item.tab === queryTab), [queryTab, menu]);
 
   useEffect(() => {
     queryTab = searchParams.get("tab") || "";
@@ -25,8 +32,7 @@ function Statistic() {
   return (
     <Container className={cx("wrapper")}>
       <TabsContainer menu={menu} />
-      {queryTab === menu[0].tab && <LikeViewStat />}
-      {queryTab === menu[1].tab && <IncomeStat />}
+      <Component />
     </Container>
   );
 }
