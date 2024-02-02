a970d2c8b6cedcd0a69ce3cfca2499e4f6d6ea36
Colpno <gvinhh@gmail.com>
2024-02-03 00:36:49 +0700

    check amount of balance before taking action

--- client/src/pages/Withdraw/components/PayPalForm.jsx
+++ client/src/pages/Withdraw/components/PayPalForm.jsx
@@ -3,14 +3,26 @@ import { FastField, Form, Formik } from "formik";
 import { Button } from "components";
 import { usePayoutPayPal } from "hooks/index.jsx";
 import { FormLabel, InputField } from "libs/formik";
+import { useSelector } from "react-redux";
 
 function PayPalForm() {
   const { payout } = usePayoutPayPal();
+  const user = useSelector((state) => state.user.user);
+
+  const canWithdraw = (amount) => {
+    if (user.income - Number.parseFloat(amount) >= 0) {
+      return true;
+    }
+    return false;
+  };
 
   const handleSubmit = (values, { setSubmitting }) => {
-    const { receiver, amount } = values;
-    payout(receiver, amount);
     setSubmitting(false);
+    const { receiver, amount } = values;
+
+    if (canWithdraw(amount)) {
+      payout(receiver, amount);
+    }
   };
 
   return (
