671a8abe217a6acb2f516a5c3f10c2bf2ef1b8ef
Colpno <gvinhh@gmail.com>
2024-02-05 04:41:26 +0700

    remove unused

--- admin/src/layouts/index.jsx
+++ admin/src/layouts/index.jsx
@@ -1,4 +1,3 @@
 export { default as AdminLayout } from "./AdminLayout/AdminLayout";
 export { default as BlankLayout } from "./BlankLayout/BlankLayout";
-export { default as DefaultLayout } from "./DefaultLayout/DefaultLayout";
 export { default as LoginLayout } from "./LoginLayout/LoginLayout";
