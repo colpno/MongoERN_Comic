7d735c429ab7e9451e7e92c4a5e99971a070ad84
Colpno <gvinhh@gmail.com>
2024-02-05 13:17:00 +0700

    render conditionally

--- admin/src/features/Loading/Loading.jsx
+++ admin/src/features/Loading/Loading.jsx
@@ -1,10 +1,15 @@
 import classNames from "classnames/bind";
-
+import { useSelector } from "react-redux";
 import styles from "./Loading.module.scss";
 
 const cx = classNames.bind(styles);
 
 function Loading() {
+  const isLoading = useSelector((state) => state.common.isLoading);
+
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  if (!isLoading) return <></>;
+
   return (
     <div className={cx("wrapper")}>
       <div className={cx("loader")}>
