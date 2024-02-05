e96f6b33c345b800a1da64e009949953f7b97527
Colpno <gvinhh@gmail.com>
2024-02-05 14:17:34 +0700

    refactor

--- admin/src/pages/Titles/Titles.jsx
+++ admin/src/pages/Titles/Titles.jsx
@@ -1,8 +1,14 @@
 import { Col, Container, Row } from "react-bootstrap";
 
 import { FloatingContainer } from "components";
-import { useGetApprovedStatuses, useGetObjectStatuses, useGetTitles, useUpdateTitle } from "hooks";
-import { StatTitleApproval, StatTitleStatus, TitleManagementCards, TitleTable } from "./components";
+import { useGetApprovedStatuses, useGetTitles } from "hooks";
+import {
+  StatTitleApproval,
+  StatTitleReleaseDay,
+  StatTitleStatus,
+  TitleManagementCards,
+  TitleTable,
+} from "./components";
 
 function Titles() {
   const { data: titles } = useGetTitles({
@@ -12,21 +18,9 @@ function Titles() {
     ]),
     _fields: "-cover.cloud_public_id -__v -_guid",
   });
-  const { data: objectStatuses } = useGetObjectStatuses({
-    _fields: "code status",
-  });
   const { data: approvedStatuses } = useGetApprovedStatuses({
     _fields: "code status color",
   });
-  const { update: updateTitle } = useUpdateTitle();
-
-  const handleUpdate = (data, setRowIdError) => {
-    const { _id, approved_status_id: approvedStatusId } = data;
-
-    updateTitle({ id: _id, data: { approved_status_id: approvedStatusId } }).catch(() => {
-      setRowIdError(_id);
-    });
-  };
 
   return (
     <Container fluid="md">
@@ -34,49 +28,36 @@ function Titles() {
         <TitleManagementCards titles={titles} />
       </Row>
       <Row>
-        <Col>
-          <h4>All Titles</h4>
-        </Col>
-      </Row>
-      <Row>
         <Col xs={12} lg={8}>
+          <h4>All Titles</h4>
           <FloatingContainer>
-            <TitleTable
-              titles={titles}
-              approvedStatuses={approvedStatuses}
-              objectStatuses={objectStatuses}
-              onUpdate={handleUpdate}
-            />
+            <TitleTable titles={titles} approvedStatuses={approvedStatuses} />
           </FloatingContainer>
         </Col>
         <Col xs={12} lg={4}>
           <Row>
             <Col>
-              {titles.length > 0 && (
-                <FloatingContainer>
-                  <StatTitleApproval titles={titles} approvedStatuses={approvedStatuses} />
-                </FloatingContainer>
-              )}
+              <FloatingContainer>
+                <StatTitleApproval titles={titles} approvedStatuses={approvedStatuses} />
+              </FloatingContainer>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <FloatingContainer>
+                <StatTitleStatus titles={titles} />
+              </FloatingContainer>
             </Col>
           </Row>
           <Row>
             <Col>
               {titles.length > 0 && (
                 <FloatingContainer>
-                  <StatTitleStatus titles={titles} statuses={objectStatuses} />
+                  <StatTitleReleaseDay titles={titles} />
                 </FloatingContainer>
               )}
             </Col>
           </Row>
-          {/* <Row>
-              <Col>
-                {titles.length > 0 && (
-                  <FloatingContainer>
-                    <StatTitleReleaseDay titles={titles} />
-                  </FloatingContainer>
-                )}
-              </Col>
-            </Row> */}
         </Col>
       </Row>
     </Container>
--- admin/src/pages/Titles/components/StatTitleReleaseDay.jsx
+++ admin/src/pages/Titles/components/StatTitleReleaseDay.jsx
@@ -3,35 +3,29 @@ import { useMemo } from "react";
 
 import { convertTimeLabel, DAYS_OF_WEEK } from "constants/time.constant";
 import { BarChart } from "features";
-import { moveArray } from "utils";
 import { getChartColors } from "utils/constants";
 
 function StatTitleReleaseDay({ titles }) {
-  const initData = useMemo(() => Array(7).fill(0), []);
   const daysOfWeek = useMemo(() => DAYS_OF_WEEK.map((day) => day.shortLabel), []);
-
   const chartData = useMemo(() => {
     return titles.reduce(
       (result, title) => {
-        if (title.approved_status_id.code === "apd" && title.status_id.code === "vis") {
-          const day = convertTimeLabel(title.release_day, "shortLabel", "number");
-          result.datasets[0].data[day]++;
-        }
+        const day = convertTimeLabel(title.release_day, "shortLabel", "number");
+        result.datasets[0].data[day]++;
         return result;
       },
       {
         labels: daysOfWeek,
         datasets: [
           {
-            label: "Số truyện chưa hoàn thành đăng vào các ngày trong tuần",
-            data: initData,
+            label: "Số lượng truyện theo ngày",
+            data: Array(7).fill(0),
             backgroundColor: getChartColors().backgroundColors.slice(13, 20),
           },
         ],
       }
     );
-  }, []);
-  moveArray(chartData.datasets[0].data, 0, 6);
+  }, [titles]);
 
   return <BarChart data={chartData} />;
 }
