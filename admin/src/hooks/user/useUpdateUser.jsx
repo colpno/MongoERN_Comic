6576b8bc4f3091ae15c4a54367c5eb7b5420b4c9
Colpno <gvinhh@gmail.com>
2024-02-05 17:20:34 +0700

    only update user in redux if update corresponding account

--- admin/src/hooks/user/useUpdateUser.jsx
+++ admin/src/hooks/user/useUpdateUser.jsx
@@ -2,19 +2,22 @@ import { useUpdateUserMutation } from "api/user.api";
 import { setLoading } from "libs/redux/slices/common.slice";
 import { setUser } from "libs/redux/slices/user.slice";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function useUpdateUser() {
   const dispatch = useDispatch();
   const [update, response] = useUpdateUserMutation();
   const { isLoading, isSuccess, data } = response;
+  const user = useSelector((state) => state.user.user);
 
   const updateClientUser = (newData) => {
     dispatch(setUser(newData));
   };
 
   useEffect(() => {
-    if (isSuccess) updateClientUser(data);
+    if (isSuccess && data._id === user._id && data.username && user.username) {
+      updateClientUser(data);
+    }
   }, [isSuccess]);
 
   useEffect(() => {
