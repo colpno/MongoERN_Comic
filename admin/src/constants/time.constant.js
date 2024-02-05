e8ac756312f62ed4f9cb2960ac5ee221343fbc29
Colpno <gvinhh@gmail.com>
2024-02-05 14:17:52 +0700

    change order of days in week

--- admin/src/constants/time.constant.js
+++ admin/src/constants/time.constant.js
@@ -1,11 +1,11 @@
 const DAYS_OF_WEEK = [
-  { shortLabel: "T2", label: "Thứ Hai", number: 1 },
-  { shortLabel: "T3", label: "Thứ Ba", number: 2 },
-  { shortLabel: "T4", label: "Thứ Tư", number: 3 },
-  { shortLabel: "T5", label: "Thứ Năm", number: 4 },
-  { shortLabel: "T6", label: "Thứ Sáu", number: 5 },
-  { shortLabel: "T7", label: "Thứ Bảy", number: 6 },
-  { shortLabel: "CN", label: "Chủ nhật", number: 0 },
+  { shortLabel: "T2", label: "Thứ Hai", number: 0 },
+  { shortLabel: "T3", label: "Thứ Ba", number: 1 },
+  { shortLabel: "T4", label: "Thứ Tư", number: 2 },
+  { shortLabel: "T5", label: "Thứ Năm", number: 3 },
+  { shortLabel: "T6", label: "Thứ Sáu", number: 4 },
+  { shortLabel: "T7", label: "Thứ Bảy", number: 5 },
+  { shortLabel: "CN", label: "Chủ nhật", number: 6 },
 ];
 
 const convertTimeLabel = (value, from, to) => {
