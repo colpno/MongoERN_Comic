365064fc1b6c3ad852576721a704fb1c1406eabd
Colpno <gvinhh@gmail.com>
2024-02-08 18:29:11 +0700

    add more condition to next button

--- client/src/layouts/ReadingLayout/components/ReadingNav.jsx
+++ client/src/layouts/ReadingLayout/components/ReadingNav.jsx
@@ -1,10 +1,28 @@
 import PropTypes from "prop-types";
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { IoChevronBackCircleOutline, IoChevronForwardCircleOutline } from "react-icons/io5";
 
 import { Button } from "components";
+import { useGetChapterTransactions } from "hooks/index.jsx";
 
 function ReadingNav({ cx, chapter, totalChapter, titleId }) {
+  const { data: chapterTransactions = [] } = useGetChapterTransactions({
+    title_id: titleId,
+    _embed: JSON.stringify([{ collection: "chapter_id", fields: "order" }]),
+  });
+  const [readLimit, setReadLimit] = useState(1);
+  const canNext = chapter.order < readLimit || chapter.order >= totalChapter;
+
+  useEffect(() => {
+    if (chapterTransactions.length > 0) {
+      const limit = chapterTransactions.reduce((previous, { chapter_id: chapterId }) => {
+        if (chapterId.order > previous) return chapterId.order;
+        return previous;
+      }, chapterTransactions[0].chapter_id.order);
+      setReadLimit(limit);
+    }
+  }, [chapterTransactions]);
+
   return (
     <div className={cx("reading-header__nav")}>
       <Button
@@ -19,13 +37,14 @@ function ReadingNav({ cx, chapter, totalChapter, titleId }) {
       </span>
       <Button
         text
-        to={`/comic/title/${titleId}/${
-          chapter.order < totalChapter ? chapter.order + 1 : totalChapter
-        }`}
-        className={cx(
-          "reading-header__nav__right",
-          chapter.order >= totalChapter ? "disabled" : ""
-        )}
+        to={
+          canNext
+            ? `/comic/title/${titleId}/${
+                chapter.order < totalChapter ? chapter.order + 1 : totalChapter
+              }`
+            : undefined
+        }
+        className={cx("reading-header__nav__right", !canNext ? "disabled" : "")}
       >
         <IoChevronForwardCircleOutline />
       </Button>
